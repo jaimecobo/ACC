22d3fed0db9c45102379e5f87aa5c2136216b494
Jaime J Cobo <jaimecobo@outlook.com>
2022-05-11 00:25:42 -0500

    Chessboard.java

---
+++ INTRO_1003/Chessboard.java
@@ -0,0 +1,32 @@
+/* Write a Java program called Chessboard that prints the following pattern to the console:
+
+
+* * * * * * * *
+ * * * * * * * *
+* * * * * * * *
+ * * * * * * * *
+* * * * * * * *
+ * * * * * * * *
+* * * * * * * *
+ * * * * * * * *
+ 
+*/
+public class Chessboard{
+
+public static void main (String [] args){
+	
+	int chessboardSize = 8;
+	int chessboardMiddle = 4;
+	for (int i = 0; i<= chessboardSize; i++){
+		for (int j = 0; j<= chessboardS; i++){
+			System.out.print("* ");
+		}
+		System.out.println();
+	
+	for (int i = 0; i<= chessboardSize; i++){
+		System.out.println(" *");
+}
+	System.out.println();
+}
+
+}
