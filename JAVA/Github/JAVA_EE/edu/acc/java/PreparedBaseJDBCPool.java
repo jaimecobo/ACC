3c45246fd2aa490b7acf14ad57a1277970d803a1
Jaime Cobo <jaimecobo@outlook.com>
2022-09-25 23:40:17 -0500

    PreparedBaseJDBCPool.java

---
+++ JAVA/Github/JAVA_EE/edu/acc/java/PreparedBaseJDBCPool.java
@@ -0,0 +1,26 @@
+package edu.acc.java;
+
+	/**
+	 * 
+	 * Utility JBDC accessor class.
+	 * This version supports PreparedStatement and Connection pooling.
+	 *
+	 */
+
+public interface PreparedBaseJDBCPool extends PreparedBaseJDBC {
+
+	/**
+	 * 
+	 * @param user valid MySQL user
+	 */
+	@Deprecated
+	void setUser(String user);
+
+	/**
+	 * 
+	 * @param pswd MySQL password for user
+	 */
+	@Deprecated
+	void setPswd(String pswd);
+
+}
