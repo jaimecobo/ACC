27386dd794aa70a1d55b4e58da85f2b135bec2b6
Jaime Cobo <jaimecobo@outlook.com>
2022-10-30 14:03:50 -0500

    fourth commit DBUtil.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/DBUtil.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/DBUtil.java
@@ -1,9 +1,14 @@
 package edu.acc.java
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
 public class DBUtil {
+	
 	private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hotels360");
+	
 		public static EntityManagerFactory getEmFactory() {
 			return emf;
 		}
+		
 }
