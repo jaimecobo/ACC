eeb4d1805f598484a82e8371482d7a9469fc4b86
Jaime Cobo <jaimecobo@outlook.com>
2022-11-02 16:59:02 -0500

    third commit Room.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Room.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Room.java
@@ -2,8 +2,17 @@ package edu.acc.java;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+@Entity
 public class Room implements Serializable {
 	
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
