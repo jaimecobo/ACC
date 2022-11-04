98fa91b90a37226ebcf19aca2c8c36e37c95f792
Jaime Cobo <jaimecobo@outlook.com>
2022-11-04 14:53:51 -0500

    fourteenth commit CreditCard_Payment.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/CreditCard_Payment.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/CreditCard_Payment.java
@@ -50,4 +50,74 @@ public class CreditCard_Payment implements Serializable {
 		this.amount = amount;
 	}
 
+
+	protected long getId_CreditCard_Payment() {
+		return id_CreditCard_Payment;
+	}
+
+
+	protected void setId_CreditCard_Payment(long id_CreditCard_Payment) {
+		this.id_CreditCard_Payment = id_CreditCard_Payment;
+	}
+
+
+	protected Customer getCustomer() {
+		return customer;
+	}
+
+
+	protected void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+
+	protected Reservation getReservation() {
+		return reservation;
+	}
+
+
+	protected void setReservation(Reservation reservation) {
+		this.reservation = reservation;
+	}
+
+
+	protected CreditCard getCreditCard() {
+		return creditCard;
+	}
+
+
+	protected void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+
+
+	protected Date getPayment_Date() {
+		return payment_Date;
+	}
+
+
+	protected void setPayment_Date(Date payment_Date) {
+		this.payment_Date = payment_Date;
+	}
+
+
+	protected String getVerification_Number() {
+		return verification_Number;
+	}
+
+
+	protected void setVerification_Number(String verification_Number) {
+		this.verification_Number = verification_Number;
+	}
+
+
+	protected double getAmount() {
+		return amount;
+	}
+
+
+	protected void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
 }
