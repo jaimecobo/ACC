be0b9eecadc42f9fe63974eeca1468efc521c09b
Jaime Cobo <jaimecobo@outlook.com>
2022-11-05 14:31:56 -0500

    fourth commit Cash_Payment.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Cash_Payment.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Cash_Payment.java
@@ -3,6 +3,9 @@ package edu.acc.java;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
 @Entity
@@ -12,5 +15,8 @@ public class Cash_Payment implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id_Cash_Payment;
 	
 }
