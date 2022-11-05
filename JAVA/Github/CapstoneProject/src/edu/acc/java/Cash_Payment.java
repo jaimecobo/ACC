9330ae7ae7faee72b98bac6f060f3c014c328d22
Jaime Cobo <jaimecobo@outlook.com>
2022-11-05 14:29:23 -0500

    second commit Cash_Payment.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Cash_Payment.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Cash_Payment.java
@@ -1,5 +1,12 @@
 package edu.acc.java;
 
-public class Cash_Payment {
+import java.io.Serializable;
 
+public class Cash_Payment implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 }
