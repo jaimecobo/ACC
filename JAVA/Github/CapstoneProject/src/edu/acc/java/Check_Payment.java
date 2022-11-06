8c5ad997fe6e4a250ea31b5c6e15d407b52247e0
Jaime Cobo <jaimecobo@outlook.com>
2022-11-06 10:37:40 -0600

    third commit Check_Payment.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Check_Payment.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Check_Payment.java
@@ -2,11 +2,11 @@ package edu.acc.java;
 
 import java.io.Serializable;
 
+@Entity
 public class Check_Payment implements Serializable{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
 }
