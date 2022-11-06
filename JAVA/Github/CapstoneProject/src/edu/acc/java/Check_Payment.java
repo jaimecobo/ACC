e0247b865a78058193ddca53fa9387ee677aa97f
Jaime Cobo <jaimecobo@outlook.com>
2022-11-06 10:38:58 -0600

    fourth commit Check_Payment.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Check_Payment.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Check_Payment.java
@@ -2,6 +2,11 @@ package edu.acc.java;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class Check_Payment implements Serializable{
 	
@@ -9,4 +14,7 @@ public class Check_Payment implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id_Check_Payment;
 }
