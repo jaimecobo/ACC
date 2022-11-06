9c6671d83851eba1e28f06b4f8ecf821a3e87add
Jaime Cobo <jaimecobo@outlook.com>
2022-11-06 10:40:33 -0600

    fifth commit Check_Payment.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Check_Payment.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Check_Payment.java
@@ -2,10 +2,12 @@ package edu.acc.java;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Check_Payment implements Serializable{
@@ -17,4 +19,6 @@ public class Check_Payment implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id_Check_Payment;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Customer customer;
 }
