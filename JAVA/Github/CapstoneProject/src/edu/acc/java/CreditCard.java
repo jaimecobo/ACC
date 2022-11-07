9191b0e84dae04075462233076bd61705a17b4a8
Jaime Cobo <jaimecobo@outlook.com>
2022-11-07 16:13:50 -0600

    fourth commit CreditCard.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/CreditCard.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/CreditCard.java
@@ -3,6 +3,9 @@ package edu.acc.java;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class CreditCard implements Serializable{
@@ -11,5 +14,8 @@ public class CreditCard implements Serializable{
 		 * 
 		 */
 		private static final long serialVersionUID = 1L;
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private long id_CreditCard;
 		
 }
