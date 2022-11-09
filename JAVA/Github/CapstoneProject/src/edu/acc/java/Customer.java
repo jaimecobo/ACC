7c3cba0763aa740ab72ccccbfbfc4c6ae2b35c17
Jaime Cobo <jaimecobo@outlook.com>
2022-11-08 22:45:59 -0600

    eleventh commit Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Customer.java
@@ -3,6 +3,8 @@ package edu.acc.java;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -23,6 +25,8 @@ public class Customer implements Serializable{
 	private String password;					//TODO Encryption
 	private String address;
 	private String email;
+	@Enumerated(EnumType.STRING)
+	private Document_Type document_Type;		//{PASSPORT, DRIVER_LICENSE, ID_CARD, OTHER}
 	
 }
 
