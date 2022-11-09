6ae1e73e8e4ef75c25c291e15f0040ca728089d6
Jaime Cobo <jaimecobo@outlook.com>
2022-11-08 22:36:58 -0600

    fourth commit Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Customer.java
@@ -3,6 +3,9 @@ package edu.acc.java;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Customer implements Serializable{
@@ -11,6 +14,9 @@ public class Customer implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id_Customer;
 	
 }
 
