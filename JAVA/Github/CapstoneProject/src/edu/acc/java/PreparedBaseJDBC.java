0e8f7970020d6b7f2a1d985b9630309845dc14e5
Jaime Cobo <jaimecobo@outlook.com>
2022-11-10 15:26:02 -0600

    third commit PreparedBaseJDBC.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/PreparedBaseJDBC.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/PreparedBaseJDBC.java
@@ -15,4 +15,13 @@ public interface PreparedBaseJDBC extends BaseJDBC {
 	@Deprecated
 	ResultSet executeQuery(String query) throws SQLException;
 	
+	/**
+	 * 
+	 * @param query the SQL syntax query to execute
+	 * @param parms the SQL parameters to substitute
+	 * @return ResultSet for given query 
+	 * @throws SQLException
+	 */
+	ResultSet executeQuery(String query, String[] parms) throws SQLException;
+
 }
