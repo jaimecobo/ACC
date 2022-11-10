51d62739bf92b893f4c3c310026a5052a9cb75c4
Jaime Cobo <jaimecobo@outlook.com>
2022-11-10 15:25:33 -0600

    second commit PreparedBaseJDBC.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/PreparedBaseJDBC.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/PreparedBaseJDBC.java
@@ -1,5 +1,18 @@
 package edu.acc.java;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/**
+ * 
+ * Utility JBDC accessor class.
+ * This version supports PreparedStatement
+ *
+ */
 public interface PreparedBaseJDBC extends BaseJDBC {
+
+	@Deprecated
+	ResultSet executeQuery(String query) throws SQLException;
 	
 }
