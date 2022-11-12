a48f754f5f042fe1d1d3b7d12f1113b9f15d433f
Jaime Cobo <jaimecobo@outlook.com>
2022-11-12 11:28:39 -0600

    third commit ConnectionPool.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/ConnectionPool.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/ConnectionPool.java
@@ -1,6 +1,9 @@
 package edu.acc.java;
 
+import javax.sql.DataSource;
+
 public class ConnectionPool {
 	private static ConnectionPool pool = null;
-	
+	private static DataSource dataSource = null;
+
 }
