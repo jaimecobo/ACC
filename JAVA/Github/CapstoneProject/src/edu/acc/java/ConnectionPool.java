771eca48c6824f92020a8e8b936fdb1300027c62
Jaime Cobo <jaimecobo@outlook.com>
2022-11-12 11:30:04 -0600

    fourth commit ConnectionPool.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/ConnectionPool.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/ConnectionPool.java
@@ -1,9 +1,21 @@
 package edu.acc.java;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class ConnectionPool {
 	private static ConnectionPool pool = null;
 	private static DataSource dataSource = null;
+	
+	private ConnectionPool(){
+		try {
+			InitialContext ic = new InitialContext();
+			dataSource = (DataSource) ic.lookup("java:/comp/env/jdbc/hotels360");	
+		}
+		catch(NamingException e){
+			System.out.println(e);
+		}
+	}
 
 }
