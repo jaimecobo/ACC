07d085736ff59f693f17eb79ad81cf2094891069
Jaime Cobo <jaimecobo@outlook.com>
2022-11-13 02:20:39 -0600

    fifth commit LocalMySQLCP.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/LocalMySQLCP.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/LocalMySQLCP.java
@@ -7,5 +7,6 @@ public class LocalMySQLCP implements PreparedBaseJDBC {
 	private Connection conn;  // database Connection
 	private String myDB = "jdbc:mysql://localhost:3306";  // local connection string
 	private String user = "root";  // local user
-	
+	private String pswd = "sesame"; // local MySQL root passwd
+
 }
