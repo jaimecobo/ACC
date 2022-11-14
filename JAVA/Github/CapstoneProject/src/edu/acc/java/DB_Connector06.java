3b18272005b66cf268149753e42c5a695cc51329
Jaime Cobo <jaimecobo@outlook.com>
2022-11-14 02:59:45 -0600

    tenth commit DB_Connector06.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/DB_Connector06.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/DB_Connector06.java
@@ -37,7 +37,11 @@ public class DB_Connector06 extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Starting the DB_Connector06");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
 		
+		LocalMySQLCP localDB = new LocalMySQLCP();
 		/*
 		String city = "Dallas";
 		String population = "1400000";
@@ -51,11 +55,33 @@ public class DB_Connector06 extends HttpServlet{
 		    parms = new String[]{city, population};  
 			rowCount = localDB.executeUpdate(sql, parms);
 		*/
+		String hotel_name = "Four";
+		String hotel_address = "4444 address 4444";
+		String hotel_id = "13";
+		String hotel_email = "four@hotels360.com";
+		String hotel_description = "This is the Four hotel";
+		String hotel_type = "1";
+		String num_of_floors = "8";
+		String num_of_rooms = "40";
+		//"insert into hotel (hotel_id, hotel_name, hotel_address, hotel_email, hotel_description, hotel_type, num_of_floors, num_of_rooms) values (?,?,?,?,?,?,?,?)";
+		String sql = "insert into hotel (hotel_id, hotel_name, hotel_address, hotel_email, hotel_description, hotel_type, num_of_floors, num_of_rooms) values (?,?,?,?,?,?,?,?)";
+		String[] parms;
 		
+		int rowCount;
+		try {
+			out.println("<br>" + hotel_name + " not found -- inserting --");
+			sql = "insert into hotel (hotel_id, hotel_name, hotel_address, hotel_email, hotel_description, hotel_type, num_of_floors, num_of_rooms ) values (?,?,?,?,?,?,?,?)";
+		    parms = new String[]{hotel_id, hotel_name, hotel_address, hotel_email, hotel_description, hotel_type, num_of_floors, num_of_rooms};  
+			rowCount = localDB.executeUpdate(sql, parms);
 			
 			
 		//localDB.executeUpdate(updateCities);
-		
+		}catch(SQLException ex){
+			out.println("ERROR catch DB_Connector06");
+			ex.printStackTrace();
+			ex.printStackTrace();
+			localDB.printTrace(ex);
+		}
 	}
 
 	/**
