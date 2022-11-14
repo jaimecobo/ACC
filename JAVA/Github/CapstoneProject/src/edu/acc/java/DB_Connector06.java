84d4cf5ff4d53c4f2e14e5b2351c5aafc3428b6c
Jaime Cobo <jaimecobo@outlook.com>
2022-11-14 02:44:00 -0600

    third commit DB_Connector06.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/DB_Connector06.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/DB_Connector06.java
@@ -1,7 +1,12 @@
 package edu.acc.java;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+/**
+ * Servlet implementation class DB_Connector06
+ */
+@WebServlet("/DB_Connector06")
 public class DB_Connector06 extends HttpServlet{
 	
 }
