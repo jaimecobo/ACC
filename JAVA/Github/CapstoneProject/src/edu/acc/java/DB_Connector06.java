635d3a0627fa63c2b06fee3ce4c6e27cd921cf40
Jaime Cobo <jaimecobo@outlook.com>
2022-11-14 02:50:56 -0600

    sixth commit DB_Connector06.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/DB_Connector06.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/DB_Connector06.java
@@ -1,7 +1,12 @@
 package edu.acc.java;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class DB_Connector06
@@ -17,5 +22,12 @@ public class DB_Connector06 extends HttpServlet{
         super();
         // TODO Auto-generated constructor stub
     }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
 
 }
