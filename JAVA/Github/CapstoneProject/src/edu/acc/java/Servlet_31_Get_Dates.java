550bda3b7a687e2f980aadc88f781f88abd23342
Jaime Cobo <jaimecobo@outlook.com>
2022-11-15 17:28:16 -0600

    sixth commit Servlet_31_Get_Dates

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_31_Get_Dates.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_31_Get_Dates.java
@@ -3,12 +3,19 @@ package edu.acc.java;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-
 /**
  * THIS SERVLET GETS DATE_FROM AND DATE_TO FROM A HTML FORM AND CALCULATES THE NUMBER OF NIGHTS
  */
 @WebServlet("/Servlet_Get_Dates")
 public class Servlet_31_Get_Dates extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet_31_Get_Dates() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 }
