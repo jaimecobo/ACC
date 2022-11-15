2c7fe9039cd3d13db27ea781bf0acd871a34a674
Jaime Cobo <jaimecobo@outlook.com>
2022-11-15 17:36:00 -0600

    eighth commit Servlet_31_Get_Dates

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_31_Get_Dates.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_31_Get_Dates.java
@@ -31,4 +31,13 @@ public class Servlet_31_Get_Dates extends HttpServlet {
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+	}
+
 }
