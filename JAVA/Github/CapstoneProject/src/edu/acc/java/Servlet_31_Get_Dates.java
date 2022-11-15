916d423f05a51c75b1a6edaa3d3f85c87af7a8e9
Jaime Cobo <jaimecobo@outlook.com>
2022-11-15 17:34:25 -0600

    seventh commit Servlet_31_Get_Dates

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_31_Get_Dates.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_31_Get_Dates.java
@@ -1,7 +1,12 @@
 package edu.acc.java;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * THIS SERVLET GETS DATE_FROM AND DATE_TO FROM A HTML FORM AND CALCULATES THE NUMBER OF NIGHTS
@@ -17,5 +22,13 @@ public class Servlet_31_Get_Dates extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
 
 }
