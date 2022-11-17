2627e7b94fe061aa76357cfe412311df5a755145
Jaime Cobo <jaimecobo@outlook.com>
2022-11-16 23:11:49 -0600

    sixth commit Servlet_51_Login01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_51_Login01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_51_Login01.java
@@ -1,7 +1,14 @@
 package edu.acc.java;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class ServletLogin01
@@ -18,5 +25,17 @@ public class Servlet_51_Login01 extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 *
+	 */
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        request.getRequestDispatcher("Login_Form.jsp").forward(request, response);
+        
+    }
+    
 }
 
