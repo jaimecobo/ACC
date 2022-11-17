0d9647f30f5e026af387a1c1ca4271020a44c750
Jaime Cobo <jaimecobo@outlook.com>
2022-11-16 23:06:33 -0600

    third commit Servlet_51_Login01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_51_Login01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_51_Login01.java
@@ -1,8 +1,13 @@
 package edu.acc.java;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+/**
+ * Servlet implementation class ServletLogin01
+ */
+@WebServlet("/ServletLogin01")
 public class Servlet_51_Login01 extends HttpServlet {
-
+	
 }
 
