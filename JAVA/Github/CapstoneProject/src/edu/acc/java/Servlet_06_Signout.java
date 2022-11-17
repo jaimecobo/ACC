67f2ca0984399e74446c2f393725f5ee4d869b77
Jaime Cobo <jaimecobo@outlook.com>
2022-11-17 08:18:22 -0600

    third commit Servlet_06_Signout.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_06_Signout.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_06_Signout.java
@@ -1,7 +1,12 @@
 package edu.acc.java;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+/**
+ * Servlet implementation class Servlet_06_Signout
+ */
+@WebServlet("/sign_Out")
 public class Servlet_06_Signout extends HttpServlet {
 
 }
