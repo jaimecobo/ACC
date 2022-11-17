540a8bf8d529c7d13798c089837cf7bcb2313dff
Jaime Cobo <jaimecobo@outlook.com>
2022-11-16 23:12:55 -0600

    eighth commit Servlet_51_Login01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_51_Login01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_51_Login01.java
@@ -2,7 +2,11 @@ package edu.acc.java;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -35,6 +39,46 @@ public class Servlet_51_Login01 extends HttpServlet {
 //        PrintWriter out = response.getWriter();
 //        request.getRequestDispatcher("Login_Form.jsp").forward(request, response);
         
+    }
+    
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+    	response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+//        LocalMySQLCP localDB = new LocalMySQLCP();
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        out.println("Username = " + username);
+		out.print("<br>");
+		out.println("Password = " + password);	
+		
+//        String username = "admin";
+//        String password = "strongpassword";
+       
+        /*
+    		try {
+    			Class.forName("com.mysql.jdbc.Driver");
+		        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotels360", "root", "sesame");
+		        PreparedStatement pst = conn.prepareStatement("Select username, password from user where username=? and password=?");
+		        pst.setString(1, username);
+		        pst.setString(2, password);
+		        ResultSet rs = pst.executeQuery();
+		        if (rs.next()) {
+		           out.println("Correct login credentials");
+		        } 
+		        else {
+		           out.println("Incorrect login credentials");
+		        }
+		    }
+    		catch (ClassNotFoundException | SQLException ex) {
+		        	System.out.println("ERROR in Catch");
+		        	ex.printStackTrace();
+		    }
+*/
     }
     
 }
