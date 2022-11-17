1e50a3576b8c8b4a6aa41ccb89716180b9f80c97
Jaime Cobo <jaimecobo@outlook.com>
2022-11-16 23:08:48 -0600

    fifth commit Servlet_51_Login01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_51_Login01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_51_Login01.java
@@ -9,6 +9,14 @@ import javax.servlet.http.HttpServlet;
 @WebServlet("/ServletLogin01")
 public class Servlet_51_Login01 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-      
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet_51_Login01() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
 }
 
