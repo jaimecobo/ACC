c9541d66c4f06a2e4a9623e180bfe9f7ea2c9b09
Jaime Cobo <jaimecobo@outlook.com>
2022-11-18 21:48:36 -0600

    third commit Servlet_18_Customers_Info.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_18_Customers_Info.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_18_Customers_Info.java
@@ -1,7 +1,12 @@
 	package edu.acc.java;
 
+	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
 
+	/**
+	 * Servlet implementation class Servlet_Management
+	 */
+	@WebServlet("/Customers_Info")
 	public class Servlet_18_Customers_Info extends HttpServlet {
-	
+		
 	}
