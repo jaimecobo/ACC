102c96a14f5d57c24ee5bf2db6a7abe79677a7a2
Jaime Cobo <jaimecobo@outlook.com>
2022-11-18 21:52:10 -0600

    sixth commit Servlet_18_Customers_Info.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_18_Customers_Info.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_18_Customers_Info.java
@@ -1,8 +1,12 @@
 	package edu.acc.java;
 
+	import java.io.IOException;
+
+	import javax.servlet.ServletException;
 	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
-
+	import javax.servlet.http.HttpServletRequest;
+	import javax.servlet.http.HttpServletResponse;
 
 	/**
 	 * Servlet implementation class Servlet_Management
@@ -18,5 +22,15 @@
 	        super();
 	        // TODO Auto-generated constructor stub
 	    }
+
+		/**
+		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+//			response.getWriter().append("Served at: ").append(request.getContextPath());
+			
+			request.getRequestDispatcher("05_management_Options.jsp").forward(request, response);
+		}
 
 	}
