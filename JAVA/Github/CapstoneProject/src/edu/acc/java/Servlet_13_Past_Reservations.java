64a9bc971f62585c89824d8d75212129247e5a37
Jaime Cobo <jaimecobo@outlook.com>
2022-11-19 22:00:46 -0600

    fifth commit Servlet_13_Past_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_13_Past_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_13_Past_Reservations.java
@@ -9,5 +9,13 @@
 	@WebServlet("/Past_Reservations")
 	public class Servlet_13_Past_Reservations extends HttpServlet {
 		private static final long serialVersionUID = 1L;
-	    
+	       
+	    /**
+	     * @see HttpServlet#HttpServlet()
+	     */
+	    public Servlet_13_Past_Reservations() {
+	        super();
+	        // TODO Auto-generated constructor stub
+	    }
+
 	}
