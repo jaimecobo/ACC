798051fb03a6252613c7e2527a3a2a4e9fbe109f
Jaime Cobo <jaimecobo@outlook.com>
2022-11-19 21:57:55 -0600

    second commit Servlet_13_Past_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_13_Past_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_13_Past_Reservations.java
@@ -1,5 +1,7 @@
    	package edu.acc.java;
 
-	public class Servlet_13_Past_Reservations {
+	import javax.servlet.http.HttpServlet;
 
+	public class Servlet_13_Past_Reservations extends HttpServlet {
+		
 	}
