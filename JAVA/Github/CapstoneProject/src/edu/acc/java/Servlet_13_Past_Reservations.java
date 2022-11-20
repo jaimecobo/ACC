cfcab848c4817ed162f4d3d9cbbc8a32dbbb6922
Jaime Cobo <jaimecobo@outlook.com>
2022-11-19 21:58:51 -0600

    third commit Servlet_13_Past_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_13_Past_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_13_Past_Reservations.java
@@ -1,7 +1,12 @@
    	package edu.acc.java;
 
+	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
 
+	/**
+	 * Servlet implementation class Servlet_Management
+	 */
+	@WebServlet("/Past_Reservations")
 	public class Servlet_13_Past_Reservations extends HttpServlet {
-		
+	
 	}
