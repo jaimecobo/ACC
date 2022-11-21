1c6573f3d8ae434e582a563acbd8a0fbdaf795fd
Jaime Cobo <jaimecobo@outlook.com>
2022-11-21 17:10:12 -0600

    third commit Servlet_15_All_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_15_All_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_15_All_Reservations.java
@@ -1,7 +1,13 @@
     package edu.acc.java;
 
+	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
 
+	/**
+	 * Servlet implementation class Servlet_Management
+	 */
+	@WebServlet("/All_Reservations")
 	public class Servlet_15_All_Reservations extends HttpServlet {
 		
+
 	}
