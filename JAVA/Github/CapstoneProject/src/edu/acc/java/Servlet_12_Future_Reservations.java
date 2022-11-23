d11e555848b95ebdeb3167508f3c970a0a2d745d
Jaime Cobo <jaimecobo@outlook.com>
2022-11-23 00:16:45 -0600

    fourth commit Servlet_12_Future_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_12_Future_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_12_Future_Reservations.java
@@ -3,10 +3,12 @@
 	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
 
+
 	/**
 	 * Servlet implementation class Servlet_Management
 	 */
 	@WebServlet("/Future_Reservations")
 	public class Servlet_12_Future_Reservations extends HttpServlet {
-		
+		private static final long serialVersionUID = 1L;
+
 	}
