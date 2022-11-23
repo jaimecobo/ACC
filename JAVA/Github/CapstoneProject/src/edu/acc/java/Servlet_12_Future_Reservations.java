e6738d8e65420cf590e4f8833e3ece3efeb31163
Jaime Cobo <jaimecobo@outlook.com>
2022-11-23 00:15:39 -0600

    third commit Servlet_12_Future_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_12_Future_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_12_Future_Reservations.java
@@ -1,7 +1,12 @@
    	package edu.acc.java;
 
+	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
 
+	/**
+	 * Servlet implementation class Servlet_Management
+	 */
+	@WebServlet("/Future_Reservations")
 	public class Servlet_12_Future_Reservations extends HttpServlet {
 		
 	}
