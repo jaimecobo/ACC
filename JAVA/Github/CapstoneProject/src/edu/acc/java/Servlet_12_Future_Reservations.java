c67661b9e21412f5c874e2c248a53eb745feb720
Jaime Cobo <jaimecobo@outlook.com>
2022-11-24 11:18:02 -0600

    ninth commit Servlet_12_Future_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_12_Future_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_12_Future_Reservations.java
@@ -1,6 +1,7 @@
    	package edu.acc.java;
 
 	import java.io.IOException;
+	import java.io.PrintWriter;
 
 	import javax.servlet.ServletException;
 	import javax.servlet.annotation.WebServlet;
@@ -38,7 +39,13 @@
 		 */
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			// TODO Auto-generated method stub
+			doGet(request, response);
 			
+			PrintWriter out  = response.getWriter();	
+			response.setContentType("text/html");
+
+			String stat = "null";
+						
 		}
 
 	}
