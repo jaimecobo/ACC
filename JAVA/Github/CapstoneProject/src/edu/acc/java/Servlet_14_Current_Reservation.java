6a11e139b1659aefc6b92062deda6ce9090cd02c
Jaime Cobo <jaimecobo@outlook.com>
2022-11-25 11:39:01 -0600

    third commit Servlet_14_Current_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_14_Current_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_14_Current_Reservation.java
@@ -1,7 +1,12 @@
 	package edu.acc.java;
 
+	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
 
+	/**
+	 * Servlet implementation class Servlet_Management
+	 */
+	@WebServlet("/Current_Reservation")
 	public class Servlet_14_Current_Reservation extends HttpServlet {
 
 	}
