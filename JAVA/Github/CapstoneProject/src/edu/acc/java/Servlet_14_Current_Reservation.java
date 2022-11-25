a834c5f39d5a32e965c4c1943f958296cb6113b9
Jaime Cobo <jaimecobo@outlook.com>
2022-11-25 11:40:14 -0600

    fourth commit Servlet_14_Current_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_14_Current_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_14_Current_Reservation.java
@@ -3,10 +3,12 @@
 	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
 
+
 	/**
 	 * Servlet implementation class Servlet_Management
 	 */
 	@WebServlet("/Current_Reservation")
 	public class Servlet_14_Current_Reservation extends HttpServlet {
+		private static final long serialVersionUID = 1L;
 
 	}
