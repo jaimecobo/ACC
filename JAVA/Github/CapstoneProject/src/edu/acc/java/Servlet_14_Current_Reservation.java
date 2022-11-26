34df4ac2121d1e4f3ce3fa688446c2d5ccfabad3
Jaime Cobo <jaimecobo@outlook.com>
2022-11-26 12:47:11 -0600

    eighth commit Servlet_14_Current_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_14_Current_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_14_Current_Reservation.java
@@ -1,7 +1,11 @@
 	package edu.acc.java;
 
 	import java.io.IOException;
+	import java.io.PrintWriter;
+	import java.sql.PreparedStatement;
+	import java.sql.SQLException;
 
+	import javax.servlet.RequestDispatcher;
 	import javax.servlet.ServletException;
 	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
@@ -34,8 +38,32 @@
 		/**
 		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 		 */
+		
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+			
+			PrintWriter out  = response.getWriter();	
+
+			/* GETS THE RETURN VALUE FROM SERVLET SPECIFIED IN STATEMENT AND ASSIGN ITS VALUE TO "DISPACHER"*/
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Servlet_ArrayOfBlockingDates");dispatcher.include(request, response);
+			Object attributeValue = request.getAttribute("result");
+			System.out.println("Array[String] Dates toString() = " + attributeValue.toString());
+
+			String stat = "null";
+			
+			
+			try {
+		        PreparedStatement pst = conn.prepareStatement("SELECT * FROM reservation WHERE departure_Date < '" + current_Date + "'");
+		        PreparedStatement pst = conn.prepareStatement("Select * from reservation_dates where status is null");
+		        pst.setString(1, stat);
+		        
+			}
+			catch (ClassNotFoundException | SQLException ex) {
+		        	System.out.println("ERROR in Catch");
+		        	ex.printStackTrace();
+		    }
+			
+			
 		}
 
 	}
