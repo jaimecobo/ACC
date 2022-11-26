fcad03ada536d147879bc1ebdc7107dae0f0d82d
Jaime Cobo <jaimecobo@outlook.com>
2022-11-26 12:39:15 -0600

    fifth commit Servlet_14_Current_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_14_Current_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_14_Current_Reservation.java
@@ -10,5 +10,13 @@
 	@WebServlet("/Current_Reservation")
 	public class Servlet_14_Current_Reservation extends HttpServlet {
 		private static final long serialVersionUID = 1L;
+	       
+	    /**
+	     * @see HttpServlet#HttpServlet()
+	     */
+	    public Servlet_14_Current_Reservation() {
+	        super();
+	        // TODO Auto-generated constructor stub
+	    }
 
 	}
