df44755446756bed0cb69a89e0d241a90808fbcf
Jaime Cobo <jaimecobo@outlook.com>
2022-11-27 14:56:55 -0600

    third commit Servlet_16_Query_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_16_Query_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_16_Query_Reservations.java
@@ -1,7 +1,12 @@
 package edu.acc.java;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+/**
+ * Servlet implementation class Servlet_16_Query_Reservations
+ */
+@WebServlet("/Servlet_16_Query_Reservations")
 public class Servlet_16_Query_Reservations extends HttpServlet {
 
 }
