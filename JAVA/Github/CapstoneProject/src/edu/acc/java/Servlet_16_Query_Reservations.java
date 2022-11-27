89846d2917dcdc64a0fa8b0a582755a0e0ff78cb
Jaime Cobo <jaimecobo@outlook.com>
2022-11-27 15:02:35 -0600

    seventh commit Servlet_16_Query_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_16_Query_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_16_Query_Reservations.java
@@ -28,7 +28,7 @@ public class Servlet_16_Query_Reservations extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 	}
 
