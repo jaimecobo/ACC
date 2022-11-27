b9a97af9bb2ca0b0fe85dedd614f060bf56c6d59
Jaime Cobo <jaimecobo@outlook.com>
2022-11-27 15:00:20 -0600

    fifth commit Servlet_16_Query_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_16_Query_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_16_Query_Reservations.java
@@ -3,12 +3,21 @@ package edu.acc.java;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+
 /**
  * Servlet implementation class Servlet_16_Query_Reservations
  */
 @WebServlet("/Servlet_16_Query_Reservations")
 public class Servlet_16_Query_Reservations extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet_16_Query_Reservations() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 }
 
