442267dfb9060da4c62c75f7c9c472bcad4d68b5
Jaime Cobo <jaimecobo@outlook.com>
2022-11-28 13:52:45 -0600

    twentieth commit Servlet_16_Query_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_16_Query_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_16_Query_Reservations.java
@@ -82,13 +82,19 @@ public class Servlet_16_Query_Reservations extends HttpServlet {
 		        	request.getSession().setAttribute("query_Reservations_String", query_Reservations_String);
 			        response.sendRedirect("16_Query_Reservations.jsp");
 	        	}
+	        	else if ((status.get(0).equals("Pending")) | (status.get(0).equals("null"))) {
+	        		query_Reservations_String = "Reservation N° : \" " + id_reservation + " \"  From: " + entryDates.get(0) + "  To: " + departureDates.get(0) + " is pending.";
+		        	request.getSession().setAttribute("query_Reservations_String", query_Reservations_String);
+			        response.sendRedirect("16_Query_Reservations.jsp");
+			        
+	        	}
+				
 	        }
-	        
+		        
 		}
 		catch (ClassNotFoundException | SQLException ex) {
 	        	System.out.println("ERROR in Catch");
 	        	ex.printStackTrace();
-				
 	    }
 		
 	}
