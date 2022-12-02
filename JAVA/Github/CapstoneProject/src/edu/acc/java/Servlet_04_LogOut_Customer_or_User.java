fa0ad61bd532e0741f4b430ea94f8e5540e612b1
Jaime Cobo <jaimecobo@outlook.com>
2022-12-02 02:10:09 -0600

    fourth commit Servlet_04_LogOut_Customer_or_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_LogOut_Customer_or_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_LogOut_Customer_or_User.java
@@ -3,12 +3,12 @@ package edu.acc.java;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-
 /**
  * Servlet implementation class Servlet_04_LogOut_Customer
  */
 @WebServlet("/LogOut")
 public class Servlet_04_LogOut_Customer_or_User extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	
 }
 
