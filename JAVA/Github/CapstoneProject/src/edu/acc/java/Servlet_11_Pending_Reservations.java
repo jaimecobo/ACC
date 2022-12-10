d972a0e2f23809fa3404acf2b79e754abd09bbf8
Jaime Cobo <jaimecobo@outlook.com>
2022-12-10 14:34:32 -0600

    fifth commit Servlet_11_Pending_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
@@ -3,6 +3,7 @@ package edu.acc.java;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+
 /**
  * Servlet implementation class Servlet_Management
  */
@@ -10,6 +11,14 @@ import javax.servlet.http.HttpServlet;
 public class Servlet_11_Pending_Reservations extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet_11_Pending_Reservations() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
 }
 
 
