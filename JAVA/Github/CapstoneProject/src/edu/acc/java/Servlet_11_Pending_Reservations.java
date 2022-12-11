c12ee56988d5789a2e497796f1c3ed234afbbdcd
Jaime Cobo <jaimecobo@outlook.com>
2022-12-11 14:38:54 -0600

    seventh commit Servlet_11_Pending_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
@@ -28,8 +28,9 @@ public class Servlet_11_Pending_Reservations extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
+		request.getRequestDispatcher("05_management_Options.jsp").forward(request, response);
 	}
 
 }
