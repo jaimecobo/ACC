991b1bfe01f0508d27753b7428142f281e920203
Jaime Cobo <jaimecobo@outlook.com>
2022-12-13 18:11:26 -0600

    ninth commit Servlet_11_Pending_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
@@ -1,12 +1,14 @@
 package edu.acc.java;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Servlet_Management
@@ -38,7 +40,10 @@ public class Servlet_11_Pending_Reservations extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-
+		doGet(request, response);
+		
+		PrintWriter out  = response.getWriter();	
+		
 	}
 
 }
