68341bb463a6a4f2b99b4641922bf4adac539532
Jaime Cobo <jaimecobo@outlook.com>
2022-12-15 11:47:06 -0600

    twelfth commit Servlet_11_Pending_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
@@ -2,12 +2,17 @@ package edu.acc.java;
 
 import java.io.IOException;
 //import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Servlet_Management
@@ -45,6 +50,18 @@ public class Servlet_11_Pending_Reservations extends HttpServlet {
 		response.setContentType("text/html");
 
 		String stat = "Pending";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotels360", "root", "sesame");
+	        PreparedStatement pst = conn.prepareStatement("Select * from reservation_dates");
+	        pst.setString(1, stat);
+	        	
+		}
+		catch (ClassNotFoundException | SQLException ex) {
+	        	System.out.println("ERROR in Catch");
+	        	ex.printStackTrace();
+	    }
 		
 	}
 
