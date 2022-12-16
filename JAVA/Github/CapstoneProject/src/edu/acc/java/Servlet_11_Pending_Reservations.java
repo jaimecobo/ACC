c4bbb570eabd62f4533c0fbee7fd01521dd03392
Jaime Cobo <jaimecobo@outlook.com>
2022-12-16 01:18:19 -0600

    fifteenth commit Servlet_11_Pending_Reservations.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
@@ -50,13 +51,23 @@ public class Servlet_11_Pending_Reservations extends HttpServlet {
 		response.setContentType("text/html");
 
 		String stat = "Pending";
-	
+		
 		try {
+			List<String> id_Customer = new ArrayList<String>();
+			List<String> id_reservation_dates = new ArrayList<String>();
+			List<String> entryDates = new ArrayList<String>();
+			List<String> departureDates = new ArrayList<String>();
+			List<String> status = new ArrayList<String>();
+			List<String> Balance = new ArrayList<String>();
+			StringBuilder strBuilder = new StringBuilder();
+			String reservation_string = "";
+	
 			Class.forName("com.mysql.jdbc.Driver");
 	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotels360", "root", "sesame");
 //	        PreparedStatement pst = conn.prepareStatement("Select * from reservation_dates");
 	        PreparedStatement pst = conn.prepareStatement("Select * from reservation where status=?");
 	        pst.setString(1, stat);
+	        ResultSet rs = pst.executeQuery();
 	        
 		}
 		catch (ClassNotFoundException | SQLException ex) {
