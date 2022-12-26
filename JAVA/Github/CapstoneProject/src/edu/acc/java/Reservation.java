83456c9e0f495ee96a3cc4d2c68fd1a249d587ec
Jaime Cobo <jaimecobo@outlook.com>
2022-12-25 23:45:48 -0600

    third commit Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Reservation.java
@@ -2,11 +2,14 @@ package edu.acc.java;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Reservation implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 }
