67d9b4e288f59d0206b1d05c2ebf78f6ecedfcad
Jaime Cobo <jaimecobo@outlook.com>
2022-12-27 00:56:50 -0600

    fifth commit Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Reservation.java
@@ -2,10 +2,12 @@ package edu.acc.java;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Reservation implements Serializable {
@@ -17,5 +19,7 @@ public class Reservation implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id_Reservation;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private int id_Customer;
 
 }
