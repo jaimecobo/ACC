5f5572727b7229048a5f1da0b3e268516e528203
Jaime Cobo <jaimecobo@outlook.com>
2023-01-07 19:24:21 -0600

    third commit Servlet_11_Pending_Reservations_01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
@@ -1,7 +1,12 @@
 package edu.acc.java;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+/**
+ * Servlet implementation class Servlet_Management
+ */
+@WebServlet("/Pending_Reservations_01")
 public class Servlet_11_Pending_Reservations_01 extends HttpServlet {
-	
+
 }
