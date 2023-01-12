5edcd5d14b23405efce06e75de884c935fb7b3c2
Jaime Cobo <jaimecobo@outlook.com>
2023-01-12 03:05:24 -0600

    fortieth commit Servlet_11_Pending_Reservations_01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
@@ -68,6 +68,7 @@ public class Servlet_11_Pending_Reservations_01 extends HttpServlet {
 	        	reservation.setDeparture_Date(departureDates.get(i));
 	        	reservation.setEntry_Date(entryDates.get(i));
 	        	reservation.setId_Customer((Integer.parseInt(id_Customer.get(i))));
+	        	reservation.setBalance((Double.parseDouble(Balance.get(i))));
 	        	
 	        }
 	        	        	
