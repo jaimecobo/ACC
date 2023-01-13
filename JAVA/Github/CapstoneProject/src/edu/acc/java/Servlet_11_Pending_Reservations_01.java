44c0faad1b888e28c93bd9a1ec7d32fff4253262
Jaime Cobo <jaimecobo@outlook.com>
2023-01-13 04:27:09 -0600

    forty-fifth commit Servlet_11_Pending_Reservations_01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
@@ -84,4 +84,12 @@ public class Servlet_11_Pending_Reservations_01 extends HttpServlet {
 	    }
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
