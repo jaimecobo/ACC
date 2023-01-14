91da3e7bd7ad5301aec37f75875e94cf9b023572
Jaime Cobo <jaimecobo@outlook.com>
2023-01-14 05:55:25 -0600

    sixty-fifth commit Servlet_11_Pending_Reservations_01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
@@ -104,7 +104,7 @@ public class Servlet_11_Pending_Reservations_01 extends HttpServlet {
 			List<String> departureDates = new ArrayList<String>();
 			List<String> status = new ArrayList<String>();
 			List<String> Balance = new ArrayList<String>();
-			StringBuilder strBuilder = new StringBuilder();
+//			StringBuilder strBuilder = new StringBuilder();
 			String reservation_string = "";
 	        	
 		}
