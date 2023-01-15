79a9db8e3bcbb740d3d06aefb3468c32c442c0cd
Jaime Cobo <jaimecobo@outlook.com>
2023-01-15 10:17:23 -0600

    seventieth commit Servlet_11_Pending_Reservations_01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
@@ -111,6 +111,7 @@ public class Servlet_11_Pending_Reservations_01 extends HttpServlet {
 	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotels360", "root", "sesame");
 //	        PreparedStatement pst = conn.prepareStatement("Select * from reservation_dates");      
 	        	
+			
 		}
 		catch (ClassNotFoundException | SQLException ex) {
 	        	System.out.println("ERROR in Catch");
