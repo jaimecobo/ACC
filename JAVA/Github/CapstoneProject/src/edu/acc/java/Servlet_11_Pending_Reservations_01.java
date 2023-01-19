713513b97a551b252400175b11a580b0ff3f6990
Jaime Cobo <jaimecobo@outlook.com>
2023-01-19 08:34:49 -0600

    one hundred fourteenth commit Servlet_11_Pending_Reservations_01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
@@ -145,6 +145,7 @@ public class Servlet_11_Pending_Reservations_01 extends HttpServlet {
 	        	reservation.getStatus() + "</td><td>" + reservation.getBalance() + "</td> <td>");
 	        	
 	        	i++;
+	        	System.out.println("3333333333333333333333333333333333333");
 	        	
 	        }
 	        
