38d5676fdd0e529e7f62417370fa0be40e3488eb
Jaime Cobo <jaimecobo@outlook.com>
2023-01-19 08:24:09 -0600

    hundred and eighth commit Servlet_11_Pending_Reservations_01.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_11_Pending_Reservations_01.java
@@ -135,6 +135,7 @@ public class Servlet_11_Pending_Reservations_01 extends HttpServlet {
 	        	Reservation reservation = new Reservation(	Long.parseLong(rs.getString(1)), 
 	        												rs.getInt(4),
 	        												rs.getString(3),
+	        												rs.getString(2),
 	        											);
 	        	
 	        }
