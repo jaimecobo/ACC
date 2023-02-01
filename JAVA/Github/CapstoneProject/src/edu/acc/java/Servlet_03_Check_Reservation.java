91730e3f81ff526eee51f8348233364f8d4e31aa
Jaime Cobo <jaimecobo@outlook.com>
2023-02-01 02:20:01 -0600

    sixth commit Servlet_03_Check_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Reservation.java
@@ -6,6 +6,6 @@ import javax.servlet.annotation.WebServlet;
  * Servlet implementation class Servlet_Check_Reservation
  */
 @WebServlet("/Check_Reservation")
-public class Servlet_03_Check_Reservation {
+public class Servlet_03_Check_Reservation extends HttpServlet {
 
 }
