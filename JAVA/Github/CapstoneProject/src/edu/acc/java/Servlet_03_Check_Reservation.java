664bed740e29c627b03d90fcb50e0d8d9a30f8e7
Jaime Cobo <jaimecobo@outlook.com>
2023-02-05 14:08:43 -0600

    twenty-fifth commit Servlet_03_Check_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Reservation.java
@@ -28,6 +28,8 @@ public class Servlet_03_Check_Reservation extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.getWriter().append();
+		
 	}
 
 }
