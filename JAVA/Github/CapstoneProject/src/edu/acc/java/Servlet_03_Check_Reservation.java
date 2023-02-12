6589525cb6c7c7be34e9edd2dc03b7e186c54b95
Jaime Cobo <jaimecobo@outlook.com>
2023-02-12 17:50:55 -0600

    eighty-first commit Servlet_03_Check_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Reservation.java
@@ -2,6 +2,7 @@ package edu.acc.java;
 
 import java.io.IOException;
 //import java.io.PrintWriter;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
