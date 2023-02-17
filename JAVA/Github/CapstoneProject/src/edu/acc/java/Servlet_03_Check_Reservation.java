0c2e6261c24b35957f46af5f70507455a6ef4360
Jaime Cobo <jaimecobo@outlook.com>
2023-02-16 19:52:35 -0600

    one hundred eighteenth commit Servlet_03_Check_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Reservation.java
@@ -108,6 +108,7 @@ public class Servlet_03_Check_Reservation extends HttpServlet {
 	        }
 		    else {
 		        	query_Reservations_String = "Reservation N°: \"" + id_reservation + "\"  doesn't exist.";
+		        	request.setAttribute();
 	        
 			}
 	        
