23449a59da17a808572aedab3fd6b9346cc883b5
Jaime Cobo <jaimecobo@outlook.com>
2023-02-17 16:38:47 -0600

    seventh commit Servlet_03_Check_Self_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
@@ -3,6 +3,10 @@
 	import javax.servlet.annotation.WebServlet;
 	import javax.servlet.http.HttpServlet;
 	
+
+	/**
+	 * Servlet implementation class Servlet_Check_Reservation
+	 */
 	@WebServlet("/Check_Self_Reservation")
 	public class Servlet_03_Check_Self_Reservation extends HttpServlet {
 	       
