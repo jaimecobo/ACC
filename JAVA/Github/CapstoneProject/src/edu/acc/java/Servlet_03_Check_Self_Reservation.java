08319fd7f2500b38bd3c8c226bd8d13ba77ab581
Jaime Cobo <jaimecobo@outlook.com>
2023-02-18 15:41:01 -0600

    tenth commit Servlet_03_Check_Self_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
@@ -1,7 +1,7 @@
 	package edu.acc.java;
 
-	import javax.servlet.ServletException;
 	import javax.servlet.annotation.WebServlet;
+	import javax.servlet.http.HttpServlet;
 
 	/**
 	 * Servlet implementation class Servlet_Check_Reservation
@@ -10,10 +10,14 @@
 	public class Servlet_03_Check_Self_Reservation extends HttpServlet {
 		private static final long serialVersionUID = 1L;
 	       
+	    /**
+	     * @see HttpServlet#HttpServlet()
+	     */
 	    public Servlet_03_Check_Self_Reservation() {
 	        super();
 	        // TODO Auto-generated constructor stub
 	    }
 	    
+	    String id_Customer02 = "";
 	    
 	}
