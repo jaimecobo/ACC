5a3067d164cec7851957387f37bc21a68ecda808
Jaime Cobo <jaimecobo@outlook.com>
2023-02-19 12:06:04 -0600

    twenty-first commit Servlet_03_Check_Self_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
@@ -31,6 +31,7 @@
 		 */
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			// TODO Auto-generated method stub
+			response.getWriter().append();
 			
 		}
 
