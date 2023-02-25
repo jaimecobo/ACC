86384c9f98c1e5598cf3f05eeb08935ada17d517
Jaime Cobo <jaimecobo@outlook.com>
2023-02-25 07:34:28 -0600

    eighty-eighth commit Servlet_03_Check_Self_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
@@ -85,7 +85,7 @@
 			
 					Class.forName("com.mysql.jdbc.Driver");
 					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotels360", "root", "sesame");
-					PreparedStatement pst = conn.prepareStatement("Select id_Reservation");
+					PreparedStatement pst = conn.prepareStatement("Select id_Reservation, departure_date");
 
 				}
 				catch (ClassNotFoundException | SQLException ex) {
