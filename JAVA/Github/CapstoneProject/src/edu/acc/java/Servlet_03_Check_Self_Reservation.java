6eb090f05888dcf554ba2b12b57980eb58cddd8e
Jaime Cobo <jaimecobo@outlook.com>
2023-02-28 22:18:50 -0600

    one hundred seventeenth commit Servlet_03_Check_Self_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
@@ -98,7 +98,7 @@
 						status.add(rs.getString(4));
 									
 						if((status.get(i).equals("Approved"))) {
-							strBuilder.append();
+							strBuilder.append("Your reservation N° : \"");
 						}
 																
 						i++;
