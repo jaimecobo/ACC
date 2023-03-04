dbefa955e6d7347c3ff554dbd9279505463c544b
Jaime Cobo <jaimecobo@outlook.com>
2023-03-03 19:49:33 -0600

    one hundred sixty fourth commit Servlet_03_Check_Self_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_03_Check_Self_Reservation.java
@@ -112,7 +112,7 @@
 					
 					if (strBuilder == null || strBuilder.toString().equals("")) {
 						reservation_string = "You have no reservations.";
-						request;
+						request.getSession();
 						
 					}
 
