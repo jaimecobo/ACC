dd19a9708237fe4f93aa3f6d734b8522fa85734d
Jaime Cobo <jaimecobo@outlook.com>
2023-03-06 18:34:50 -0600

    sixth commit Servlet_04_Login_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
@@ -3,7 +3,7 @@ package edu.acc.java;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet()
+@WebServlet("/Login01")
 public class Servlet_04_Login_Customer extends HttpServlet {
  
 }
