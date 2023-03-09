fecd1b28207bf595f53a4f127597ee40abed7ebf
Jaime Cobo <jaimecobo@outlook.com>
2023-03-09 16:23:48 -0600

    thirty third commit Servlet_04_Login_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
@@ -37,7 +37,7 @@ public class Servlet_04_Login_Customer extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request) {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub	
 
 	}
