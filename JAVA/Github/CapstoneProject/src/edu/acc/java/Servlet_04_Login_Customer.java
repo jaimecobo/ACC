e229f872dbf5670fa6603992e3f6060b718099b9
Jaime Cobo <jaimecobo@outlook.com>
2023-03-09 16:27:29 -0600

    forty first commit Servlet_04_Login_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
@@ -40,7 +40,7 @@ public class Servlet_04_Login_Customer extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
 		
 		PrintWriter out  = response.getWriter();	
 
