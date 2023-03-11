0d3c0677bbc489c45dde0bef00a206f79fe645e7
Jaime Cobo <jaimecobo@outlook.com>
2023-03-10 20:18:31 -0600

    forty sixth commit Servlet_04_Login_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
@@ -45,6 +45,7 @@ public class Servlet_04_Login_Customer extends HttpServlet {
 //		PrintWriter out  = response.getWriter();	
 		response.setContentType("text/html");
 		DataManager dt = new DataManager();
+		HttpSession session;
 		
 	}
 
