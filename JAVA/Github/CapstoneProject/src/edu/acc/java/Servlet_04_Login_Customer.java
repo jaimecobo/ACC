da1a8e29b8a2d053399d24e75463f2fb765aefe8
Jaime Cobo <jaimecobo@outlook.com>
2023-03-15 18:29:17 -0500

    seventy eighth commit Servlet_04_Login_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
@@ -3,6 +3,7 @@ package edu.acc.java;
 import java.io.IOException;
 //import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
