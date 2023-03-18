83465b1b15a0dd36e5bdb288223ad46125949dd5
Jaime Cobo <jaimecobo@outlook.com>
2023-03-17 20:49:17 -0500

    one hundred ninth commit Servlet_04_Login_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
@@ -81,7 +81,10 @@ public class Servlet_04_Login_Customer extends HttpServlet {
 
 	        	session.setAttribute("id_Customer02", id_Customer);
 	        	out.print("<a href='Check_Self_Reservation'>visit</a>");
-	        					
+	        	
+	        	
+	        	String id_Customer02;
+				
 	        } 
 	  
 	    }
