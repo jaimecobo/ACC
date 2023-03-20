dc69f222096a9e6ae606a4cf4fb71c844af3df42
Jaime Cobo <jaimecobo@outlook.com>
2023-03-20 10:38:44 -0500

    one hundred fifty second commit Servlet_04_Login_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
@@ -1,7 +1,7 @@
 package edu.acc.java;
 
 import java.io.IOException;
-//import java.io.PrintWriter;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -48,7 +48,7 @@ public class Servlet_04_Login_Customer extends HttpServlet {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
 		
-//		PrintWriter out  = response.getWriter();	
+		PrintWriter out  = response.getWriter();	
 		response.setContentType("text/html");
 		DataManager dt = new DataManager();
 		HttpSession session = request.getSession();
