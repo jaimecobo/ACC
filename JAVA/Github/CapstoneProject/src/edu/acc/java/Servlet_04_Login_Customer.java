7f15e76b81294d367f4bbf5b23909187a1c19030
Jaime Cobo <jaimecobo@outlook.com>
2023-03-20 10:40:42 -0500

    one hundred fifty sixth commit Servlet_04_Login_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_04_Login_Customer.java
@@ -96,6 +96,7 @@ public class Servlet_04_Login_Customer extends HttpServlet {
 	        else {
 //	        	out.println("Incorrect login credentials");
 	        	String message = "Incorrect login credentials.";
+	        	request;
 	        
 	        }
 			
