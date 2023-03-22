dfe6ba3fd046a814bc6bacc91f983ab05602df8c
Jaime Cobo <jaimecobo@outlook.com>
2023-03-22 14:45:52 -0500

    sixth commit Servlet_22_Deny.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
@@ -6,6 +6,6 @@ import javax.servlet.annotation.WebServlet;
  * Servlet implementation class Servlet_Deny
  */
 @WebServlet("/Servlet_Deny")
-public class Servlet_22_Deny {
+public class Servlet_22_Deny extends HttpServlet {
 
 }
