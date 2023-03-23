94af36ab7b3f83e5783ec7da75a7502feebc87e5
Jaime Cobo <jaimecobo@outlook.com>
2023-03-23 11:52:41 -0500

    ninth commit Servlet_22_Deny.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
@@ -10,5 +10,9 @@ import javax.servlet.http.HttpServlet;
 @WebServlet("/Servlet_Deny")
 public class Servlet_22_Deny extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    public Servlet_22_Deny() {
+		
+    }
 
 }
