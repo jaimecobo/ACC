85f3c9af9dd6cf15185f9ec93d3ce0de7dac07fb
Jaime Cobo <jaimecobo@outlook.com>
2023-03-23 20:36:37 -0500

    nineteenth commit Servlet_22_Deny.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
@@ -21,6 +21,9 @@ public class Servlet_22_Deny extends HttpServlet {
 		
     }
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
 	}
