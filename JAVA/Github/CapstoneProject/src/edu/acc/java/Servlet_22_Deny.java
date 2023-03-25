953df3499944c2292e1a5af555098b289e258fe8
Jaime Cobo <jaimecobo@outlook.com>
2023-03-24 20:42:02 -0500

    twenty eighth commit Servlet_22_Deny.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
@@ -29,7 +29,7 @@ public class Servlet_22_Deny extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter();
+		response.getWriter().append();
 
 	}
 
