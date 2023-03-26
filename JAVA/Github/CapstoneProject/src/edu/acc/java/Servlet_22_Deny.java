67e2f68d378757ee11ab67463680af7b13e5d641
Jaime Cobo <jaimecobo@outlook.com>
2023-03-26 15:45:37 -0500

    fifty fifth commit Servlet_22_Deny.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
@@ -45,7 +45,7 @@ public class Servlet_22_Deny extends HttpServlet {
 		String sql;
 		String[] parms;
 		LocalMySQLCP localDB = new LocalMySQLCP();
-		DataManager dt;
+		DataManager dt = new DataManager();
 				
 	}
 
