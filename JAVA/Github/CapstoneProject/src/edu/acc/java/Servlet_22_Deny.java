e87c72a4b2d8c5e602c7883c45418dd68104b1de
Jaime Cobo <jaimecobo@outlook.com>
2023-03-27 12:15:12 -0500

    sixty eighth commit Servlet_22_Deny.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_22_Deny.java
@@ -52,7 +52,7 @@ public class Servlet_22_Deny extends HttpServlet {
 
 			sql = "update reservation set status='Denied'  WHERE id_Reservation=?";
 		    parms = new String[]{id_Reservation};  
-			rowCount = localDB;
+			rowCount = localDB.executeUpdate();
 			
 		}catch(){
 			
