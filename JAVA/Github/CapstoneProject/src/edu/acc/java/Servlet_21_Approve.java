1145f0560943e361591fd62545cd71cea045a9f5
Jaime Cobo <jaimecobo@outlook.com>
2023-03-31 16:18:58 -0500

    twenty first commit Servlet_21_Approve.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
@@ -21,7 +21,7 @@ public class Servlet_21_Approve extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest) {
+	protected void doGet(HttpServletRequest request) {
 
 	}
 
