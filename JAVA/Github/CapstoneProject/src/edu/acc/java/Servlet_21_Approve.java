b4c089a173f81cf573544492076da4df8e471e37
Jaime Cobo <jaimecobo@outlook.com>
2023-03-31 16:24:44 -0500

    thirty sixth commit Servlet_21_Approve.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
@@ -28,7 +28,7 @@ public class Servlet_21_Approve extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ");
+		response.getWriter().append("Served at: ").append();
 
 	}
 
