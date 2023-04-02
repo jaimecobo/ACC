8190a2f8da2cd18a2e16b2369f624f25ba9fd67c
Jaime Cobo <jaimecobo@outlook.com>
2023-04-01 21:53:33 -0500

    forty ninth commit Servlet_21_Approve.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
@@ -36,6 +36,7 @@ public class Servlet_21_Approve extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		
 	}
 
