20c5a5f4c4b33b68967bc95766f21a61c160914f
Jaime Cobo <jaimecobo@outlook.com>
2023-04-03 16:10:45 -0500

    eighty second commit Servlet_21_Approve.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
@@ -48,10 +48,10 @@ public class Servlet_21_Approve extends HttpServlet {
 		LocalMySQLCP localDB = new LocalMySQLCP();
 		DataManager dt = new DataManager();
 		int rowCount;
-		
 		try {
 
 			sql = "update reservation set status='Approved'  WHERE id_Reservation=?";
+		    parms;  
 			
 		}catch(){
 			
