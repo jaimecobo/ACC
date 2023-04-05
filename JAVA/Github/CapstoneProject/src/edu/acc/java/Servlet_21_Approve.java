ec5cfb0b7e7641f81bbd2e3b4023b088c411736d
Jaime Cobo <jaimecobo@outlook.com>
2023-04-04 22:47:04 -0500

    ninety second commit Servlet_21_Approve.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_21_Approve.java
@@ -58,6 +58,7 @@ public class Servlet_21_Approve extends HttpServlet {
 			
 		}catch(SQLException ex){
 			System.out.println("ERROR IN Servlet_Create_Reservation TRYING TO INSERT \"APPROVED\" STATUS IN THE DB");
+			ex;
 			
 		}finally {
 
