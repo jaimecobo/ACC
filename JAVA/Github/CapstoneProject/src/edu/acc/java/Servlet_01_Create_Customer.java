80336bfb79bfa420cb0883b99352770269a05f3f
Jaime Cobo <jaimecobo@outlook.com>
2023-04-06 06:02:25 -0500

    eleventh commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -4,7 +4,7 @@ package edu.acc.java;
 /**
  * Servlet implementation class Servlet_Create_Customer
  */
-@WebServlet()
+@WebServlet("/Create_Customer")
 public class Servlet_01_Create_Customer {
 	
 }
