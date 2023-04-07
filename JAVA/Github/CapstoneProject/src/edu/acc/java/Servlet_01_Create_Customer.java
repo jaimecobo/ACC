9859a7faa839314afa6685c9681618b98ba01a46
Jaime Cobo <jaimecobo@outlook.com>
2023-04-07 18:19:29 -0500

    seventeenth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -6,6 +6,6 @@ import javax.servlet.annotation.WebServlet;
  * Servlet implementation class Servlet_Create_Customer
  */
 @WebServlet("/Create_Customer")
-public class Servlet_01_Create_Customer {
+public class Servlet_01_Create_Customer extends {
 	
 }
