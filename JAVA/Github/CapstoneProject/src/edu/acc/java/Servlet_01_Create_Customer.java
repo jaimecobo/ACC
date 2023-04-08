3e8fa81fddd9a4c62eca63513fb32ed230e8edce
Jaime Cobo <jaimecobo@outlook.com>
2023-04-08 18:33:37 -0500

    thirty ninth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -2,7 +2,7 @@ package edu.acc.java;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import
+import javax
 
 /**
  * Servlet implementation class Servlet_Create_Customer
