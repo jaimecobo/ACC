6a24ae060d26d6ec800674dc0e402dcd4e14ea85
Jaime Cobo <jaimecobo@outlook.com>
2023-04-12 09:30:36 -0500

    ninety fourth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -39,6 +39,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		PrintWriter out  = response.getWriter();	
+		response
 
 	}
 	
