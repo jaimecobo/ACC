6f1d09ed3d45548e6c9b88c54cd5eb9261b2e3ad
Jaime Cobo <jaimecobo@outlook.com>
2023-04-12 09:37:59 -0500

    one hundred tenth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -42,7 +42,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 		response.setContentType("text/html");
 		
 		LocalMySQLCP localDB = new LocalMySQLCP();
-		DataManager dt = new
+		DataManager dt = new DataManager();
 
 	}
 	
