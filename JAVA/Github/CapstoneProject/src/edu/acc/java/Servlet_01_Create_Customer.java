0a6be33abfeb799ff010968f081b3f1fb73fe1e3
Jaime Cobo <jaimecobo@outlook.com>
2023-04-17 09:06:56 -0500

    one hundred eightieth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -2,7 +2,7 @@ package edu.acc.java;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util
+import java.util.Date
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
