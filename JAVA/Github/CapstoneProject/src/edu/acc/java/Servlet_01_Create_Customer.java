317890408cfdbcd71164f596ec3192740926796c
Jaime Cobo <jaimecobo@outlook.com>
2023-04-17 09:05:33 -0500

    one hundred seventy fifth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -51,7 +51,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 		String doc_Type = request.getParameter("doc_Type");
 		String doc_Number = request.getParameter("doc_Number");
 		String phone = request.getParameter("phone");
-		Date bd = (Date)formatter.parse()
+		Date bd = (Date)formatter.parse("birthday")
 		
 	}
 	
