2cd4750684de27a4b1c3cc639be68051348d3a65
Jaime Cobo <jaimecobo@outlook.com>
2023-04-22 16:18:38 -0500

    two hundred twentieth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -60,11 +60,11 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 		try {
 			birthday01 = (Date)formatter.parse("birthday00");
 			birthday00 = formatter.format(birthday01);
-			
-		} catch () {
 
+		} catch (ParseException) {
+		
 		} 
-
+		
 	}
 	
 }
