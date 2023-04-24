7d1ad1554ec370d762871832b92424f8a2be2967
Jaime Cobo <jaimecobo@outlook.com>
2023-04-24 09:27:53 -0500

    two hundred forty ninth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -69,7 +69,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 		
 		birthday = birthday01.toString();
 		request.getParameter(formatter.format("birthday"));
-		String company = request.getParameter("company")
+		String company = request.getParameter("company");
 
 	}
 	
