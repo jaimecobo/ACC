7d0219e4273bb7cb8be81e05c2610d6119842f3c
Jaime Cobo <jaimecobo@outlook.com>
2023-04-29 09:53:48 -0500

    three hundred twenty seventh commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -84,7 +84,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 		
 		
 		out.println("<br>" + "firstName 	: " + firstName);
-		out.println("<br>" + "lastName 		: ")
+		out.println("<br>" + "lastName 		: " + lastName)
 
 	}
 	
