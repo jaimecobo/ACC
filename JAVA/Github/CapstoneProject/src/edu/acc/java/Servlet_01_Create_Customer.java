81c26336497489c0a32e55be99b96c4cd9b3f343
Jaime Cobo <jaimecobo@outlook.com>
2023-05-09 08:38:51 -0500

    five hundred twelfth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -106,7 +106,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotels360", "root", "sesame");
-	        PreparedStatement pst = conn.prepareStatement
+	        PreparedStatement pst = conn.prepareStatement()
 	        
 	    }
 		catch() {
