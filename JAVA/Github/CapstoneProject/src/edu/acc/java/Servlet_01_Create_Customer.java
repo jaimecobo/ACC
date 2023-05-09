675cf4851a77f9a1432d59b3355a00dbedcb005b
Jaime Cobo <jaimecobo@outlook.com>
2023-05-09 08:26:00 -0500

    five hundredth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
