c235f074ad95c3b1ab8ca8db9619c2373ee6e918
Jaime Cobo <jaimecobo@outlook.com>
2023-05-11 21:42:58 -0500

    five hundred forty first commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -94,7 +94,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 		out.println("<br>" + "doc_Type 		: " + doc_Type);
 		out.println("<br>" + "doc_Number	: " + doc_Number);
 		out.println("<br>" + "phone			: " + phone);
-//		out.println("<br>" + "birthday		: " + birthday);
+		out.println("<br>" + "birthday		: " + birthday);
 //		out.println("<br>" + "company		: " + company);
 //		out.println("<br>" + "origin		: " + origin);
 //		out.println("<br>" + "occupation	: " + occupation);
