62586e0b100970ede06baf1bccf4eba3c74e9bbe
Jaime Cobo <jaimecobo@outlook.com>
2023-05-12 21:59:38 -0500

    five hundred sixty second commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.
+import java.sql.ResultSet
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
