fa9ce36aa74689ceb6803948170a9d8c2128dc6d
Jaime Cobo <jaimecobo@outlook.com>
2023-05-16 10:53:12 -0500

    six hundred fifteenth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -102,7 +102,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 	
 		int rowCount;
 		try {
-			DateFormat formatter = new SimpleDateFormat
+			DateFormat formatter = new SimpleDateFormat()
 			
 		}catch(){
 			
