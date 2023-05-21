66e91bbb575ce1639686fd5ca32be0af3bd5d85f
Jaime Cobo <jaimecobo@outlook.com>
2023-05-20 23:43:56 -0500

    six hundred sixty seventh commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -2,7 +2,7 @@ package edu.acc.java;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
