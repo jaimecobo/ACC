71763af082b0e7d1dea307e8c8bcc1f5c807d425
Jaime Cobo <jaimecobo@outlook.com>
2023-05-22 12:53:26 -0500

    six hundred eighty ninth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -117,7 +117,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 			}
 			
 			
-			sql
+			sql =
 			
 		} catch(SQLException ex){
 			out.println("ERROR catch in Servlet_Create_Customer");
