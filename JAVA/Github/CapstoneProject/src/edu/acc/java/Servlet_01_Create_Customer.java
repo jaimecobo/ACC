ef83639f9cd279150cf58c2f6de8bc949887c88c
Jaime Cobo <jaimecobo@outlook.com>
2023-05-25 13:27:03 -0500

    seven hundred fifty second commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -120,7 +120,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 			sql = "insert into customer (firstName, lastName, address, email, document_Type, document_Number, phoneNumber, birthday, company_Name, origin_Nationality, occupation, "
 					+ "account_Creation_Date, userName, password) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 		    parms = new String[]{firstName, lastName, address, email, doc_Type, doc_Number, phone, birthday, company, origin, occupation, dateCreated, userName, password};  
-			rowCount = localDB.executeUpdate()
+			rowCount = localDB.executeUpdate(sql)
 			
 		}catch(SQLException ex){
 			out.println("ERROR catch in Servlet_Create_Customer");
