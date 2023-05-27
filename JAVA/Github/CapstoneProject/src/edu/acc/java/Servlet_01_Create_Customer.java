a588706fad87f3547884a6c209f691eda71fbbc8
Jaime Cobo <jaimecobo@outlook.com>
2023-05-27 00:06:58 -0500

    seven hundred ninety second commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -64,7 +64,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 //			birthday00 = formatter.format(birthday01);
 
 //		} catch (ParseException e) {
-			// TODO Auto-generated catch block
+//			// TODO Auto-generated catch block
 			e.printStackTrace();
 
 		} 
