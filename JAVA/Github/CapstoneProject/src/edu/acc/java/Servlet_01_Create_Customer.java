f1cda41ee9307c3d0af034038586fb9964dafe59
Jaime Cobo <jaimecobo@outlook.com>
2023-05-29 09:52:03 -0500

    eight hundred thirty third commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -102,7 +102,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 		
 	
 		int rowCount;
-		try {
+//		try {
 //			DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
 //			String birthday00 = request.getParameter("birthday");
 //			Date birthday01;
