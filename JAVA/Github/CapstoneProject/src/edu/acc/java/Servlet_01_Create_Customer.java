a0edc28db735fecb9c5e7045b897240878091be1
Jaime Cobo <jaimecobo@outlook.com>
2023-06-01 12:05:08 -0500

    eight hundred eighty fourth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -104,7 +104,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-	        Connection conn = DriverManager.
+	        Connection conn = DriverManager.getConnection
 	        
 	    } catch (ClassNotFoundException | SQLException ex) {
 	        	System.out.println("ERROR in Catch");
