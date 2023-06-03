3fc5faf2e4ca45f1a14359db9e5d40774c0f4cf9
Jaime Cobo <jaimecobo@outlook.com>
2023-06-03 10:04:59 -0500

    nine hundred thirty third commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -109,7 +109,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotels360", "root", "sesame");
 	        PreparedStatement pst = conn.prepareStatement("Select username from customer where username=?");
 	        pst.setString(1, userName);
-	        ResultSet rs = pst
+	        ResultSet rs = pst.
 	        
 	    } catch (ClassNotFoundException | SQLException ex) {
 	        	System.out.println("ERROR in Catch");
