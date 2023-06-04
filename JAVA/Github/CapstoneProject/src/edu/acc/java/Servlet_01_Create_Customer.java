42cec11b068edb32ca7f028f92aa1dfabb6dce45
Jaime Cobo <jaimecobo@outlook.com>
2023-06-04 11:02:41 -0500

    nine hundred forty first commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -111,7 +111,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 	        pst.setString(1, userName);
 	        ResultSet rs = pst.executeQuery();
 	        
-	        if (rs) {
+	        if (rs.) {
 				
 	        } 
 	        
