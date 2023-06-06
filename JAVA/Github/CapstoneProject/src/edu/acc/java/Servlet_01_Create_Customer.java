b601e4eeee30c5ab5fbf6aaed711ecfbc3dc650e
Jaime Cobo <jaimecobo@outlook.com>
2023-06-06 15:08:14 -0500

    nine hundred seventy fifth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -117,6 +117,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 	        	response.sendRedirect("02_Customer_Form.jsp");
 				
 	        } 
+	        else
 	        
 	    } catch (ClassNotFoundException | SQLException ex) {
 	        	System.out.println("ERROR in Catch");
