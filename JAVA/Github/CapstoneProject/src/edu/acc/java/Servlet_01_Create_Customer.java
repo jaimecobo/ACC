7773ad56a156793739f646d243e0fb18d2026128
Jaime Cobo <jaimecobo@outlook.com>
2023-06-07 16:18:38 -0500

    one thousand fourth commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -122,6 +122,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 	    		try {	    			
 					
 	    		} catch(SQLException ex){
+	    			out
 					
 	    		} finally {
 	    			dt.close(rs);
