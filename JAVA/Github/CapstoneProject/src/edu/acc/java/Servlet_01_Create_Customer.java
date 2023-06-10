86c36c6812791c7587592c3bb0b4da3513a447f8
Jaime Cobo <jaimecobo@outlook.com>
2023-06-10 12:05:11 -0500

    one thousand fifty third commit Servlet_01_Create_Customer.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_01_Create_Customer.java
@@ -121,7 +121,7 @@ public class Servlet_01_Create_Customer extends HttpServlet {
 	        	
 	        	int rowCount;
 	    		try {	    			
-	    			sql = "insert into customer (firstName, lastName, address, email, document_Type, document_Number, phoneNumber, birthday, company_Name, origin_Nationality"
+	    			sql = "insert into customer (firstName, lastName, address, email, document_Type, document_Number, phoneNumber, birthday, company_Name, origin_Nationality,"
 					
 	    		} catch(SQLException ex){
 	    			out.println("ERROR catch in Servlet_Create_Customer");
