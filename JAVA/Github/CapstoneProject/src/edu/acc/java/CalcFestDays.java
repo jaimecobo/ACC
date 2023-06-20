2a9d64ca63cc820e283a4a78a881fb986e7dc66d
Jaime Cobo <jaimecobo@outlook.com>
2023-06-20 11:48:17 -0500

    thirty eighth commit CalcFestDays.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
@@ -3,11 +3,12 @@ package edu.acc.java;
 import java.util.Date;
 
 public class CalcFestDays {
-	
+
 	public int calcDays(Date entry_Date, Date departureDate) {
 		
 		int festDaysCount = 0;
-		
+		SimpleDateFormat
+	
 		return festDaysCount;
 		
 	}
