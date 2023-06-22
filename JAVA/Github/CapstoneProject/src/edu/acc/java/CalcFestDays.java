c8b8a8db96a878cc8aad326e03698a82d6fd590c
Jaime Cobo <jaimecobo@outlook.com>
2023-06-22 11:49:26 -0500

    eighty second commit CalcFestDays.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
@@ -13,7 +13,7 @@ public class CalcFestDays {
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 		List<Date> dates = new ArrayList<Date>();
 		
-		long 						
+		long interval 						
 		
 		return festDaysCount;
 		
