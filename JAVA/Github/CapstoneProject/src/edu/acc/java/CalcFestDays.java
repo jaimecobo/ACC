f2e5586166b1dd4dd43c7b432638705ac0626ce2
Jaime Cobo <jaimecobo@outlook.com>
2023-07-01 18:19:14 -0500

    two hundred sixth commit CalcFestDays.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
@@ -30,6 +30,7 @@ public class CalcFestDays {
 		    
 		    for(int j=0; j < festDates.length; j++){
                 
+                //
                 if(festDates[j].equals(ds)){
                 	festDaysCount++;
 					
