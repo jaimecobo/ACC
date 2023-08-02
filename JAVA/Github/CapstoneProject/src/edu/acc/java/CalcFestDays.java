16c493a744a4f2148421f33179e965f642063cbb
Jaime Cobo <jaimecobo@outlook.com>
2023-08-01 23:17:34 -0500

    four hundred thirteenth commit CalcFestDays.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
@@ -19,6 +19,7 @@ public class CalcFestDays {
 	public int calcDays(Date entry_Date, Date departureDate) {
 		
 		int festDaysCount = 0;
+		SimpleDateFormat
 		List<Date> dates = new ArrayList<Date>();
 		
 		long interval = 24 * 60 * 60 * 1000; 							// 1 day in milliseconds
