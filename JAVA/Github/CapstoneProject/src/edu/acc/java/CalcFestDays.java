335ee59fec00d0a215fdb9e1f2bc5def178efd95
Jaime Cobo <jaimecobo@outlook.com>
2023-08-08 12:14:32 -0500

    four hundred fifty commit CalcFestDays.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/CalcFestDays.java
@@ -1,6 +1,6 @@
 package edu.acc.java;
 
-import java.text.SimpleDateFormat;
+import
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,8 +19,7 @@ public class CalcFestDays {
 	
 	public int calcDays(Date entry_Date, Date departureDate) {
 		
-		int festDaysCount = 0;
-		SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+		int festDaysCount = 0;			
 		List<Date> dates = new ArrayList<Date>();
 		
 		long interval = 24 * 60 * 60 * 1000; 							// 1 day in milliseconds
