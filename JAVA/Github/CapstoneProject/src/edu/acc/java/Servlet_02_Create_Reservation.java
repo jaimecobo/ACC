250f187b13985a8ea2bacf487ddc6565c9b64e5e
Jaime Cobo <jaimecobo@outlook.com>
2023-10-06 12:07:41 -0500

    nineteenth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -3,4 +3,4 @@ package edu.acc.java;
 /**
  * Servlet implementation class Servlet_Create_Reservation
  */
-public class Servlet_02_Create_Reservation extends
+public class Servlet_02_Create_Reservation extends HttpServlet
