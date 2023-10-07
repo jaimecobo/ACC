217043b51d13daebe3b25beda0807611b8deb0c0
Jaime Cobo <jaimecobo@outlook.com>
2023-10-07 13:59:39 -0500

    thirty fifth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServlet;
 /**
  * Servlet implementation class Servlet_Create_Reservation
  */
-@WebServlet("/")
+@WebServlet("/Create_Reservation")
 public class Servlet_02_Create_Reservation extends HttpServlet {
 
 }
