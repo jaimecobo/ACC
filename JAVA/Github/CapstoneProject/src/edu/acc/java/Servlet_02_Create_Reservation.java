fce114c8957077b5acb4d5a7279b9dfaaed33aba
Jaime Cobo <jaimecobo@outlook.com>
2023-10-10 16:48:20 -0500

    seventy fifth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -22,6 +22,6 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void
+	protected void doGet
 
 }
