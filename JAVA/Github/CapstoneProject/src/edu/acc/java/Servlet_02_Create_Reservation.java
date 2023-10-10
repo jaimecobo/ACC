9a9d6257506b19a995c05ca5b9b3553181dcadd2
Jaime Cobo <jaimecobo@outlook.com>
2023-10-10 16:47:24 -0500

    seventy first commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -20,7 +20,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
     String id_Customer = "";
 
 	/**
-	 * @see HttpServlet#doGet()
+	 * @see HttpServlet#doGet(HttpServletRequest request)
 	 */
 
 }
