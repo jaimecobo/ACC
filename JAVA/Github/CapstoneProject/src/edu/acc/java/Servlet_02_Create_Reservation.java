873d4b1e596b81b35d1dcc0e49da8f441177cb03
Jaime Cobo <jaimecobo@outlook.com>
2023-10-13 00:54:48 -0500

    one hundred eleventh commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -25,6 +25,6 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 
 }
