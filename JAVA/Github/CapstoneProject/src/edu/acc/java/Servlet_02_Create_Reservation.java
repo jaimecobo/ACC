a1634366fd76ee48c3d0f364591d1ae65ab97bcc
Jaime Cobo <jaimecobo@outlook.com>
2023-10-14 01:02:28 -0500

    one hundred thirty sixth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -29,7 +29,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath)
+		response.getWriter().append("Served at: ").append(request.getContextPath())
 
 	}
 
