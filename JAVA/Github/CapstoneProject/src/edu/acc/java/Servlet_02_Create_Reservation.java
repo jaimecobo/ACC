af74b81d8cddc633e4467297067dab08726685f6
Jaime Cobo <jaimecobo@outlook.com>
2023-10-16 14:01:04 -0500

    one hundred sixtieth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -32,6 +32,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		try {
 	        response.setContentType("text/html");  
+	        PrintWriter
 		
 		}catch(Exception e){System.out.println(e);} 
 		
