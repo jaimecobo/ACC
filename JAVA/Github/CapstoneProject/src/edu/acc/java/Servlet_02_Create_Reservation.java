8ccfdb776b5774b8edc5727ffac2c0324ca6b450
Jaime Cobo <jaimecobo@outlook.com>
2023-10-17 11:34:31 -0500

    one hundred eighty seventh commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -36,7 +36,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	        response.setContentType("text/html");  
 	        PrintWriter out = response.getWriter();  
 	          
-	        HttpSession session=
+	        HttpSession session=request
 		
 		}catch(Exception e){System.out.println(e);} 
 		
