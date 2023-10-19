1cea0d63445950b25b1a5d7283968d0d09b247d3
Jaime Cobo <jaimecobo@outlook.com>
2023-10-19 17:01:41 -0500

    two hundred fifteenth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -38,7 +38,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	          
 	        HttpSession session=request.getSession(false);  
 	        id_Customer = (String)session.getAttribute("id_Customer01");
-	        System.out.println("the id_Customer is " + id_Customer + "")
+	        System.out.println("the id_Customer is " + id_Customer + "at doGet method in Create_Reservation Servlet.")
 		
 		}catch(Exception e){System.out.println(e);} 
 		
