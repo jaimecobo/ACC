2e27cff64ed943706e566c3de587a4ea12262d5f
Jaime Cobo <jaimecobo@outlook.com>
2023-10-29 15:22:13 -0500

    three hundred twenty seventh commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -68,7 +68,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 		HttpSession session=request.getSession(false);  
         id_Customer = (String)session.getAttribute("id_Customer01");
         
-        if(id_Customer !=)
+        if(id_Customer != null)
 		
 	}
 
