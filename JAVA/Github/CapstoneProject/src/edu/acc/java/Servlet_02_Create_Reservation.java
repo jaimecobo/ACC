a3023279afa4711fea027bcbb872b350605855e0
Jaime Cobo <jaimecobo@outlook.com>
2023-11-20 17:18:21 -0600

    six hundred twenty ninth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -103,6 +103,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 		out.println("<br>" + "origin		: " + origin);
 		out.println("<br>" + "occupation	: " + occupation);
 		out.println("<br>" + "dateCreated	: " + dateCreated);
+		out
 		
         }
 		
