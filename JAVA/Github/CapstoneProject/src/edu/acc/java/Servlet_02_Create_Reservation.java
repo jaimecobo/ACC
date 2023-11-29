4075aecaf976d445b0be6de118731055a85e6658
Jaime Cobo <jaimecobo@outlook.com>
2023-11-28 21:05:16 -0600

    six hundred eighty fifth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -112,7 +112,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 		
 		StringBuilder strBuilder = new StringBuilder();
 		strBuilder.append("{ ");
-		List<>
+		List<Date>
 
         }
 		
