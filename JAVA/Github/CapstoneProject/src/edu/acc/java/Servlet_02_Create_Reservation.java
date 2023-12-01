03a8280e8aa3d6964badf0dc8e0b1faafa634fed
Jaime Cobo <jaimecobo@outlook.com>
2023-11-30 21:20:55 -0600

    seven hundred thirteenth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -115,7 +115,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 		StringBuilder strBuilder = new StringBuilder();
 		strBuilder.append("{ ");
 		List<Date> dates = new ArrayList<Date>();
-		int fDaysCount
+		int festDaysCount
 		
         }
 		
