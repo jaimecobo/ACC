3106ec05ed4b1cc71d9dd46ec82e19dfad817dc3
Jaime Cobo <jaimecobo@outlook.com>
2023-12-01 13:51:50 -0600

    seven hundred thirty seventh commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -120,7 +120,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 		double total = 0.0;
 		
         }else {
-        	String loginReqMsg = "";
+        	String loginReqMsg = "<h3>";
                	
         }
 		
