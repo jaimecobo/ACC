13363ee0c93409583707cc10906177954b09e5bb
Jaime Cobo <jaimecobo@outlook.com>
2023-12-08 21:14:31 -0600

    eight hundred thirty fourth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -127,6 +127,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 			String formattedDate = formatter.format(dateStr);
 
 			} catch (ParseException e) {
+			
 			}
 
         }else {
