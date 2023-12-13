43a063e8e3e83fed10d6154f39a5df54a11c500b
Jaime Cobo <jaimecobo@outlook.com>
2023-12-13 01:22:14 -0600

    nine hundred sixteenth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -131,7 +131,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 //			formattedDate = formatter.format(date1);
 //			out.println("<br> dd-MMM-yyyy date is ==> " + formattedDate);
 			
-			formatter = new SimpleDateFormat("yyyy-MM-dd");		//this format is to test running it with blockedDates_part0123.js
+//			formatter = new SimpleDateFormat("yyyy-MM-dd");		//this format is to test running it with blockedDates_part0123.js
 			
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
