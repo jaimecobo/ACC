bc5ee1518131974d4bec77bcd50691b4db619e77
Jaime Cobo <jaimecobo@outlook.com>
2023-12-16 23:53:49 -0600

    nine hundred eighty second commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -139,7 +139,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 			
 			CalcFestDays calculate = new CalcFestDays();
 			
-			festDaysCount = calculate.
+			festDaysCount = calculate.calcDays
 
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
