560b8a62bb365dc6cfaa103e39d5d5714a230451
Jaime Cobo <jaimecobo@outlook.com>
2023-12-25 09:25:12 -0600

    one thousand one hundred forty first commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -158,7 +158,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 			    
 			    strBuilder.append("\"" + ds + "\"" + ", ");
 			    
-			    for(int j=0; j < festDates.length; i)
+			    for(int j=0; j < festDates.length; i++)
 			}
 
 			} catch (ParseException e) {
