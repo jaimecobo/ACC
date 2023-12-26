3e35c57281a6a0ab7ba46ab198b88321776bdc65
Jaime Cobo <jaimecobo@outlook.com>
2023-12-26 01:19:37 -0600

    one thousand one hundred sixtieth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -166,7 +166,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
                     }
 			    }
 			}
-			strBuilder
+			strBuilder.
 
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
