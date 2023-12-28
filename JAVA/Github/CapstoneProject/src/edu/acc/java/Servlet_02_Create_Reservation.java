95fed2180f0a85bf63422c067cb399aaf16e514f
Jaime Cobo <jaimecobo@outlook.com>
2023-12-28 10:19:42 -0600

    one thousand two hundred tenth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -176,6 +176,13 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 			System.out.println();
 			System.out.print("NUMBER OF FEST DAYS ARE: " + festDaysCount + " * FEST * FEST * FEST * FEST * FEST *");
 			System.out.println();
+			
+			
+
+			
+			
+			
+			/* Subtract dates */
 
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
