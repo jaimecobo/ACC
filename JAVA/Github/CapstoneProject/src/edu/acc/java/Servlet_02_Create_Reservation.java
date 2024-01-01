d0656c5402c80e150cf5358509354d1f4a95134b
Jaime Cobo <jaimecobo@outlook.com>
2024-01-01 15:14:22 -0600

    one thousand two hundred sixty eighth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -189,7 +189,9 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 			System.out.println();
 			System.out.println("Nights = " + diffDays);
 			System.out.println();
-
+			
+			total
+			
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
