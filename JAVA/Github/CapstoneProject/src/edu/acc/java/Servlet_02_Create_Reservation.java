b83b58b4dec1608d853c35520611f8646f14b122
Jaime Cobo <jaimecobo@outlook.com>
2024-01-06 09:22:06 -0600

    one thousand three hundred seventy first commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -201,6 +201,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 			daysAmount = "" + diffDays;
 			System.out.println(daysAmount);
 			request.setAttribute("price_msj", price_msj);
+			request
 
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
