e12740149593c634a7aa44a0e6b71ffb1be58fe2
Jaime Cobo <jaimecobo@outlook.com>
2024-01-07 09:30:45 -0600

    one thousand three hundred ninety fifth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -204,7 +204,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 			request.getRequestDispatcher("reservation_Form.jsp").forward(request, response);
 
 			
-        	request.getSession().setAttribute("price_msj", price_msj)
+        	request.getSession().setAttribute("price_msj", price_msj);
 	       
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
