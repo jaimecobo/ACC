1d395d032435e3f66052107bb41bb08196e57657
Jaime Cobo <jaimecobo@outlook.com>
2024-01-11 16:56:28 -0600

    one thousand four hundred seventy third commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -219,7 +219,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 		String errorMsg = "<h3>Selected dates are no longer available!</h3>";
 
 		try {
-			Class.forName("com.")
+			Class.forName("com.mysql")
 			
 	    }
 		catch (ClassNotFoundException | SQLException ex) {
