8076531d3b3112abb5c9618d1a145f501517602d
Jaime Cobo <jaimecobo@outlook.com>
2024-01-12 13:13:16 -0600

    one thousand four hundred eighty third commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -2,7 +2,7 @@ package edu.acc.java;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.
+import java.sql
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
