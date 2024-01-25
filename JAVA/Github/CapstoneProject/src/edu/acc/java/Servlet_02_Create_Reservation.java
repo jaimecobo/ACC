f1c3cd4ba5fd751d6289c35107e4afe3782f22c1
Jaime Cobo <jaimecobo@outlook.com>
2024-01-25 17:11:17 -0600

    one thousand seven hundred thirty first commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -242,7 +242,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	    		try {
 
 	    		    
-	    		    sql   
+	    		    sql =  
 	    			
 	    		}catch(SQLException ex){
 	    			out.println("ERROR IN Servlet_Create_Reservation TRYING TO INSERT A RESERVATION IN THE DB");
