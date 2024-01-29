b98203e053042ad80b984f5ca4a7f879e046c699
Jaime Cobo <jaimecobo@outlook.com>
2024-01-29 11:35:49 -0600

    one thousand eight hundred fourteenth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -244,7 +244,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	    		    
 	    		    sql = "insert into reservation (departure_Date, entry_Date, status, balance, id_Customer) values (?,?,?,?,?)";
 	    		    parms = new String[]{departure_Date, entry_Date, status, balance, id_Customer};  
-	    			rowCount = localDB.executeUpdate(
+	    			rowCount = localDB.executeUpdate()
 	    			
 	    		}catch(SQLException ex){
 	    			out.println("ERROR IN Servlet_Create_Reservation TRYING TO INSERT A RESERVATION IN THE DB");
