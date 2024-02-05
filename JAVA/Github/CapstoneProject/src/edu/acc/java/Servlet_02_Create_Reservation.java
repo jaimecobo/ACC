3609906c2b181701332e0f00ff21396ec36d7993
Jaime Cobo <jaimecobo@outlook.com>
2024-02-04 20:34:08 -0600

    one thousand nine hundred thirty sixth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -272,7 +272,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 		
 		try {
 			
-			List<String>
+			List<String> resrv_num
 	       
 	    }
 		catch (ClassNotFoundException | SQLException ex) {
