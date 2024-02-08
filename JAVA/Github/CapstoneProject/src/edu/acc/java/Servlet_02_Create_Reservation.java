dfaecd5d6d801c952549aff14ad2e0910d041190
Jaime Cobo <jaimecobo@outlook.com>
2024-02-08 15:34:47 -0600

    two thousand seventh commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -276,7 +276,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 			StringBuilder reservation_conf = new StringBuilder();
 	
 			Class.forName("com.mysql.jdbc.Driver");
-	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost")
+	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:")
 	       
 	    }
 		catch (ClassNotFoundException | SQLException ex) {
