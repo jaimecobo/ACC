f9916121a151c6c03d00f49aeb17416e50314ee8
Jaime Cobo <jaimecobo@outlook.com>
2024-02-20 22:42:41 -0600

    two thousand two hundred sixty third commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -286,7 +286,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	        	resrv_num.add(rs.getString(1));
 //	        	reservation_conf.append("Reservation number \" " + resrv_num.get(0) + " \" has been created.</td></tr>"
 	        	reservation_conf.append("Reservation number " + resrv_num.get(0) + " has been created.</td></tr>"
-	        			+ "<tr><td style =\"color:blue\" align:\"center\">It will be a \" " + daysAmount + " \" night stay. </td>");
+	        			+ "<tr><td style =\"color:blue\" align:\"center\">It will be a \" " + daysAmount + " \" night stay. </td><");
 
 	        }
 	       
