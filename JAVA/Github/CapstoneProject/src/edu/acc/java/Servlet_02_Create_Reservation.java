920b5e893358f489feaf271800d53a629e14c3da
Jaime Cobo <jaimecobo@outlook.com>
2024-02-28 09:55:03 -0600

    two thousand four hundred seventeenth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -290,7 +290,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	        			+ "<tr><td style =\"color:blue\" align:\"center\">Total Balance = " + total + " $US.</td></tr>");
 	        	if(festDaysCount > 0) {
 	        		reservation_conf.append("<tr><td style =\"color:red\">You had an increase of 50% in " + festDaysCount + " of the days of your stay </td></tr>"
-	        				+ " <tr><td style =\"color:red\"></td>");
+	        				+ " <tr><td style =\"color:red\"></td><");
 	        				
 	        	}
 
