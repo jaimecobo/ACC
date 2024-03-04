113f81bb7e79c213f395e9242985c3e8838d556e
Jaime Cobo <jaimecobo@outlook.com>
2024-03-04 16:20:19 -0600

    two thousand five hundred twenty eighth commit Servlet_02_Create_Reservation.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_02_Create_Reservation.java
@@ -297,7 +297,7 @@ public class Servlet_02_Create_Reservation extends HttpServlet {
 	        	}
 	        	
 	        	Java_Mailgun mailGum = new Java_Mailgun();
-	    		mailGum.sendEmail(
+	    		mailGum.sendEmail()
 	        	
 	        }
 	       
