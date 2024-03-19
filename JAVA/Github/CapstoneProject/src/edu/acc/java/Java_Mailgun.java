c4e1d88076592aba14a37f3fcd7f0033aebc5d7b
Jaime Cobo <jaimecobo@outlook.com>
2024-03-19 17:55:10 -0500

    Sixty fifth commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -3,7 +3,7 @@ package edu.acc.java;
 public class Java_Mailgun {
 	
 		
-		private static final String MAILGUN_DOMAIN_NAME = "sandboxba654c3607224f489582c989d20d5dfe.mailgun.org"
+		private static final String MAILGUN_DOMAIN_NAME = "sandboxba654c3607224f489582c989d20d5dfe.mailgun.org";
 		
 		
 		String recipient;
