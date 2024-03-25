01c823da762a33ed45aa5dd2809fa0b05423de0e
Jaime Cobo <jaimecobo@outlook.com>
2024-03-25 16:40:02 -0500

    One hundred ninety ninth commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -26,7 +26,7 @@ public class Java_Mailgun {
 			System.out.println("Starting sendEmail at Java_Mailgun");
 		 
 		 try {
-				Class.forName("com.mysql")
+				Class.forName("com.mysql.jdbc")
 	
 		 }
 			catch (ClassNotFoundException | SQLException ex) {
