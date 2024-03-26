aa8b0a95c53ccb16a76026a3248bcd9625ba4a1a
Jaime Cobo <jaimecobo@outlook.com>
2024-03-26 16:43:51 -0500

    Two hundred eleventh commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -28,7 +28,7 @@ public class Java_Mailgun {
 		 
 		 try {
 				Class.forName("com.mysql.jdbc.Driver");
-		        Connection 
+		        Connection conn
 	
 		 }
 			catch (ClassNotFoundException | SQLException ex) {
