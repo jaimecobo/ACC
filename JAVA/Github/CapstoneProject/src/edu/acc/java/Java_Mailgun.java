3d59fa6ba144ba368b40ccab9c24daec42d6bc36
Jaime Cobo <jaimecobo@outlook.com>
2024-03-27 12:41:50 -0500

    Two hundred thirty sixth commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -29,7 +29,7 @@ public class Java_Mailgun {
 		 
 		 try {
 				Class.forName("com.mysql.jdbc.Driver");
-		        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306")
+		        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/")
 	
 		 }
 			catch (ClassNotFoundException | SQLException ex) {
