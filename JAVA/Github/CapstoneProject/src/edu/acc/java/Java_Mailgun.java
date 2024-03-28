c673a48398c327c458337336bbd4ab631df0944e
Jaime Cobo <jaimecobo@outlook.com>
2024-03-28 08:52:32 -0500

    Two hundred fifty eighth commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -3,7 +3,7 @@ package edu.acc.java;
 //import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.
+import java.sql.PreparedStatement
 import java.sql.SQLException;
 
 public class Java_Mailgun {
