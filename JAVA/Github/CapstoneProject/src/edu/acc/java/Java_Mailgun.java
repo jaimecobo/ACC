e47105a8b96b35bd6058a73ca82555f668e4cd03
Jaime Cobo <jaimecobo@outlook.com>
2024-04-05 01:07:45 -0500

    Four hundred eighteenth commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -54,7 +54,7 @@ public class Java_Mailgun {
 	  
 	  private ClientResponse sendSimpleMessage(String recipient) {
 
-	    Client client = Client.create
+	    Client client = Client.create(
 	    
 	  }
 	
