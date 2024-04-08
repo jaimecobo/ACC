ed2dc3829f7de56d65e2ef83b7b472e9b4bf7107
Jaime Cobo <jaimecobo@outlook.com>
2024-04-08 17:04:18 -0500

    Four hundred sixty second commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -56,7 +56,7 @@ public class Java_Mailgun {
 
 	    Client client = Client.create();
 	    client.addFilter(new HTTPBasicAuthFilter("api", MAILGUN_API_KEY));
-	    WebResource
+	    WebResource 
 	    return webResource.type(MediaType.APPLICATION_FORM_URLENCODED).post(ClientResponse.class,
 	        formData);
 	  }
