dc451760d179550b18e636a6e41f5f60159d6812
Jaime Cobo <jaimecobo@outlook.com>
2024-04-18 17:28:58 -0500

    Six hundred sixty first commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -4,6 +4,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
+
 //import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
