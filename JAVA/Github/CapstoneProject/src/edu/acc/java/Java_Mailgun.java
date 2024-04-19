676d8289a9dd93c633c462b05760a86380328b57
Jaime Cobo <jaimecobo@outlook.com>
2024-04-19 18:35:51 -0500

    Six hundred ninety sixth commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -4,7 +4,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
-import com.sun.jersey.core
+import com.sun.jersey.core.
 
 //import java.io.IOException;
 import java.sql.Connection;
