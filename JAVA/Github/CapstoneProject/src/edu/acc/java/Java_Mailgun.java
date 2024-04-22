8a3a9ba5208e51cfa1919e3467dd37f4c7fd153e
Jaime Cobo <jaimecobo@outlook.com>
2024-04-22 18:16:24 -0500

    Seven hundred fifty seventh commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -80,7 +80,7 @@ public class Java_Mailgun {
 	  private ClientResponse sendComplexMessage(String recipient) {
 
 	    return webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE)
-	        .post(ClientResponse.)
+	        .post(ClientResponse.class)
 	  }
 	
 }
