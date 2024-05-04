bf931c68fab08635fcd6a7618582d1d55006a1e0
Jaime <jaimecobo@outlook.com>
2024-05-04 17:13:11 -0500

    Nine hundred eighteenth commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -5,7 +5,7 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
-import com.sun.jersey.
+import com.sun.jersey.multipart
 
 //import java.io.IOException;
 import java.sql.Connection;
