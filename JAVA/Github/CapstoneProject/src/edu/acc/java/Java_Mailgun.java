225bb0b2891817cf76f14ce8e00e71bcc5e19cc8
Jaime <jaimecobo@outlook.com>
2024-05-07 14:34:33 -0500

    Nine hundred thirty first commit Java_Mailgun.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Java_Mailgun.java
@@ -87,6 +87,7 @@ public class Java_Mailgun {
 	    formData.field("from", "Mailgun User <mailgun@" + MAILGUN_DOMAIN_NAME + ">");
 	    formData.field("to", recipient);
 	    formData.field("subject", "Complex Mailgun Example");
+	    formData
 	    return webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE)
 	        .post(ClientResponse.class, formData);
 	  }
