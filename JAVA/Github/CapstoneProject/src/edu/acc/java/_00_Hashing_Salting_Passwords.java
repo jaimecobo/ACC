d2184265ec79fa92bf3dc45cb2dae533288bfea0
Jaime <jaimecobo@outlook.com>
2024-05-11 15:32:15 -0500

    Twentieth commit _00_Hashing_Salting_Passwords.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
@@ -2,6 +2,6 @@ package edu.acc.java;
 
 public class _00_Hashing_Salting_Passwords {
 
-	public static String hashPassword(String password) throws
+	public static String hashPassword(String password) throws NoSuchAlgorithmException
 	
 }
