d27a557716bc4440e892f8a0a9638cd8d8884f22
Jaime <jaimecobo@outlook.com>
2024-05-13 15:52:32 -0500

    Thirty first commit _00_Hashing_Salting_Passwords.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
@@ -4,7 +4,8 @@ import java.security.NoSuchAlgorithmException;
 
 public class _00_Hashing_Salting_Passwords {
 
-	public static String hashPassword(String password) throws NoSuchAlgorithmException{}
+	public static String hashPassword(String password) throws NoSuchAlgorithmException{
+		}
 	}
 
 }
