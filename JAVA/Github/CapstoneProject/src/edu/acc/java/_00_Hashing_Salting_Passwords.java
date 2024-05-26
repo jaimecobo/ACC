3af5f99c5b904469813b340d906e378e4cfe0e96
Jaime <jaimecobo@outlook.com>
2024-05-25 20:32:25 -0500

    Two hundred twenty fourth commit _00_Hashing_Salting_Passwords.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
@@ -25,6 +25,6 @@ public class _00_Hashing_Salting_Passwords {
 	
 	public static String getSalt() {
 		Random rand = new SecureRandom();
-		byte[] saltBytes = new byte[
+		byte[] saltBytes = new byte[]
 	}	
 }
