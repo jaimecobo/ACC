91100aaa734a7d8dcd0dfc71b8ad22765eef4f20
Jaime <jaimecobo@outlook.com>
2024-06-08 00:04:25 -0500

    Four hundred thirty seventh commit _00_Hashing_Salting_Passwords.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
@@ -42,7 +42,8 @@ public class _00_Hashing_Salting_Passwords {
 	public static void checkPasswordStrength(String password) throws Exception{
 		if (password == null || password.trim().isEmpty()) {
 			throw new Exception("Password cannot be empty.");
-		}else if(password.length() < 8){}
+		}else if(password.length() < 8){
+		}
 	}
 	
 }
