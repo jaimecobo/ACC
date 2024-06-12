ac2aa8d363ec7b2929dc590eb0fbe57a3113f703
Jaime <jaimecobo@outlook.com>
2024-06-12 11:51:09 -0500

    Four hundred eighty fifth commit _00_Hashing_Salting_Passwords.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Hashing_Salting_Passwords.java
@@ -49,7 +49,8 @@ public class _00_Hashing_Salting_Passwords {
 	}
 	
 	/* This code test the functionality of this class. */
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		try		
 	}
 	
 	
