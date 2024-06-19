75cd97087ffb3a7680bd61b26d9286077398fd36
Jaime <jaimecobo@outlook.com>
2024-06-18 22:09:59 -0500

    Thirty eighth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -1,6 +1,6 @@
 package edu.acc.java;
 
 public class Servlet_17_Create_User extends HttpServlet {
-	private static final long serialVersionUID = 
+	private static final long serialVersionUID = 1
 
 }
