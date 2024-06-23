2efd54d126a1bc32dcfd078b210008002aac78fb
Jaime <jaimecobo@outlook.com>
2024-06-23 10:34:19 -0500

    One hundred twenty ninth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -1,6 +1,6 @@
 package edu.acc.java;
 
-import javax.servlet.ServletException
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
