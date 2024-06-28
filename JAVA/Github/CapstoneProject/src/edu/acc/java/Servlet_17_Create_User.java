3ba26401b04975786624a2691866ebdda2a4a448
Jaime <jaimecobo@outlook.com>
2024-06-28 00:54:44 -0500

    Two hundred thirty sixth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -36,7 +36,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 
+		// TODO Auto-generated method stub
 	
 	}
 
