284a1ba2f51cbe1f976deee5e0e79c7a21c9214f
Jaime <jaimecobo@outlook.com>
2024-07-01 12:53:45 -0500

    Two hundred ninety eighth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -45,7 +45,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 		
 		final JPanel panel = new JPanel();
 		// create a jframe
-	    JFrame frame = new JFrame("JOptionPane")
+	    JFrame frame = new JFrame("JOptionPane ")
 		
 	}
 
