7f1d2b6e2643a93b664ab7940a301c7e79cc8760
Jaime <jaimecobo@outlook.com>
2024-07-04 13:23:47 -0500

    Three hundred forty second commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -51,7 +51,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 		PrintWriter out  = response.getWriter();	
 		response.setContentType("text/html");
 		
-		String password
+		String password =
 	
 	}
 
