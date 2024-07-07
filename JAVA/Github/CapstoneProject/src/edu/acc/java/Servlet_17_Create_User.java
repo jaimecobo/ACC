938a912a7e37db7c4303f28b49efddd86519a048
Jaime <jaimecobo@outlook.com>
2024-07-07 18:09:10 -0500

    Four hundred thirteenth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -56,7 +56,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 		System.out.println("1111111111 " + password);
 		
 		try {
-			System.out.println("222222222 starting first")
+			System.out.println("222222222 starting first ")
 		
 		} catch (Exception ex) {}
 	
