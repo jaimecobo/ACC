8f2e435e92f78e5f45610a046b437394899ac621
Jaime <jaimecobo@outlook.com>
2024-07-10 15:21:46 -0500

    Four hundred seventieth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -59,7 +59,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 			System.out.println("222222222 starting first Try block 'checkPasswordStrength' -- password = " + password);
 			_00_Hashing_Salting_Passwords.checkPasswordStrength(password);
 			System.out.println("33333333 After checkPasswordStrength -- password = " + password);
-			try
+			try 
 		
 		} catch (Exception ex) {}
 	
