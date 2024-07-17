fab8d5fc1a4d6140ed95e2e4d8813ebfeb73edd9
Jaime <jaimecobo@outlook.com>
2024-07-16 19:50:05 -0500

    Five hundred ninety ninth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -65,7 +65,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 				password = _00_Hashing_Salting_Passwords.hashSaltAndPassword(password, salt);
 				System.out.println("555555555 After hashPassword -- password = " + password);
 			}catch(Exception ex) {
-				System.out.println("77777")
+				System.out.println("777777")
 			}
 		
 		} catch (Exception ex) {}
