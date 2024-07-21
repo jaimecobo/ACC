e0f2c87ff8af8a052d7a681e077a2f3e4fe505ff
Jaime <jaimecobo@outlook.com>
2024-07-20 19:59:45 -0500

    Six hundred sixty sixth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -68,6 +68,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 				System.out.println("7777777777 Exception at hashAndSaltPassword catch -- password = " + password);
 				   out.println("<script type=\"text/javascript\">");
 				   out.println("alert(" + ex + ");");
+				
 			}
 		
 		} catch (Exception ex) {}
