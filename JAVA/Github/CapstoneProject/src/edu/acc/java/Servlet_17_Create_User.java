337f6409e93d5d047ef59944353f20d791f16d7f
Jaime <jaimecobo@outlook.com>
2024-07-24 18:04:42 -0500

    Seven hundred forty second commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -2,15 +2,6 @@ package edu.acc.java;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -85,66 +76,9 @@ public class Servlet_17_Create_User extends HttpServlet {
 		
 		} catch (Exception ex) {
 			// TODO Auto-generated catch block
-			System.out.println("666666666 Exception at checkPasswordStrength catch -- password = " + password);
-			out.println("<script type=\"text/javascript\">");
-			   out.println("alert(" + ex + ");");
-			   out.println("location='17_adminUserForm.jsp';");
-			   out.println("</script>");
-//			JOptionPane.showMessageDialog(frame, ex, "Warning", JOptionPane.WARNING_MESSAGE);
-			ex.printStackTrace();
+			System.out.println()
 		}
-		
-		LocalMySQLCP localDB = new LocalMySQLCP();
-		String firstName = request.getParameter("firstName");
-		String lastName = request.getParameter("lastName");
-		String address = request.getParameter("address");
-		String email = request.getParameter("email");
-		String doc_Type = request.getParameter("doc_Type");
-		String doc_Number = request.getParameter("doc_Number");
-		String phone = request.getParameter("phone");
-		String birthday = request.getParameter("birthday");
-		String userName = request.getParameter("userName");
-		
-		String sql;
-		String[] parms;
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotels360", "root", "sesame");
-	        PreparedStatement pst = conn.prepareStatement("Select username from user where username=?");
-	        pst.setString(1, userName);
-	        ResultSet rs = pst.executeQuery();
-	        
-	        if (rs.next()) {
-	        	String otherUsername02 = " ** Please choose a different username. **";
-	        	request.getSession().setAttribute("otherUsername02", otherUsername02);
-	        	response.sendRedirect("17_adminUserForm.jsp");
-	        } 
-	        else {
-		
-				int rowCount;
-				try {
-					String userCreated;
-		
-					sql = "insert into user (firstName, lastName, address, email, document_Type, document_Number, phone_Number, birthday, username, password, salt) values (?,?,?,?,?,?,?,?,?,?,?)";
-				    parms = new String[]{firstName, lastName, address, email, doc_Type, doc_Number, phone,  birthday, userName, password, salt};  
-					rowCount = localDB.executeUpdate(sql, parms);
-					userCreated = "Username \" " + userName + " \" has been created.";
-					request.getSession().setAttribute("userCreated", userCreated);
-			        response.sendRedirect("17_adminUserForm.jsp");
-				}catch(SQLException ex){
-					out.println("ERROR catch in Servlet_Create_User");
-					ex.printStackTrace();
-					localDB.printTrace(ex);
-				}
-	        }
-	        
-		}
-		catch (ClassNotFoundException | SQLException ex) {
-	        	System.out.println("ERROR in Catch");
-	        	ex.printStackTrace();
-	    }
-	
+
 	}
 
 }
