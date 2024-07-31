a8acfd6ee987400bd37b8ba063abbf2d11d0e401
Jaime <jaimecobo@outlook.com>
2024-07-30 20:57:21 -0500

    Eight hundred sixty sixth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -82,7 +82,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 			   out.println("location='17_adminUserForm.jsp';");
 			   out.println("</script>");
 			JOptionPane.showMessageDialog(frame, ex, "Warning", JOptionPane.WARNING_MESSAGE);
-			ex.printStackTrace(
+			ex.printStackTrace()
 		}
 	
 	}
