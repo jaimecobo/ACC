b459dfe7a51aa3405b5b224ad5de8bc1a09e114f
Jaime <jaimecobo@outlook.com>
2024-08-29 13:01:49 -0500

    Two thousand sixty second commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -102,7 +102,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-	        
+	        Connection
 	        
 		}
 		catch (ClassNotFoundException | SQLException ex) {
