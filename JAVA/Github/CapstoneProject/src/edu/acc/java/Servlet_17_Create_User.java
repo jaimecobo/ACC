0243606af4fce49ee045db9266195db3d879a539
Jaime <jaimecobo@outlook.com>
2024-08-31 07:48:20 -0500

    Three thousand sixth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -104,7 +104,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost",)
+	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:",)
 	        
 		}
 		catch (ClassNotFoundException | SQLException ex) {
