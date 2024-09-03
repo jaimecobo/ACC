e459b54c291ffe00f8a38ec7b64fdd3844808a1e
Jaime <jaimecobo@outlook.com>
2024-09-03 18:18:34 -0500

    Three thousand sixty sixth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -107,7 +107,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 			Class.forName("com.mysql.jdbc.Driver");
 	        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotels360", "root", "sesame");
 	        PreparedStatement pst = conn.prepareStatement("Select username from user where username=?");
-	        pst.setString(
+	        pst.setString()
 	        
 		}
 		catch (ClassNotFoundException | SQLException ex) {
