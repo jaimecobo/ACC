896ec87df3652cf3619f9c0c571106914c23c49c
Jaime <jaimecobo@outlook.com>
2024-09-03 18:20:26 -0500

    Three thousand seventy sixth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import
+import 
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
