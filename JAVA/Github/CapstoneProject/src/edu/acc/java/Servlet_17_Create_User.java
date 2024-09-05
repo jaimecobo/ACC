1d4ebc2d1d8cc4c9cd6ae6605397417751feeeef
Jaime <jaimecobo@outlook.com>
2024-09-05 09:15:45 -0500

    Three thousand one hundred second commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -112,6 +112,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 	        ResultSet rs = pst.executeQuery();
 	        
 	        
+	        
 		}
 		catch (ClassNotFoundException | SQLException ex) {
 	        	System.out.println("ERROR in Catch");
