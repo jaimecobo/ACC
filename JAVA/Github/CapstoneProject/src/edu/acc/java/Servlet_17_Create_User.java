0c751b095576f5d2f237c4209d5fce871507e348
Jaime <jaimecobo@outlook.com>
2024-09-07 19:34:04 -0500

    Three thousand one hundred fiftieth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -113,7 +113,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 	        
 	        if (rs.next()) {
 	        	String otherUsername02 = " ** Please choose a different username. **";
-	        	request.getSession()
+	        	request.getSession().
 	        } 
 	        
 		}
