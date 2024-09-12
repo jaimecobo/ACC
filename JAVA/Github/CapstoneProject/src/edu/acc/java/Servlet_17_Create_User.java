7099d44740014790640a4419d04f1dede1b213cc
Jaime <jaimecobo@outlook.com>
2024-09-11 21:36:25 -0500

    Three thousand two hundred twenty fifth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -123,6 +123,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 					
 				}catch(SQLException ex){
 					out.println("ERROR catch in Servlet_Create_User");
+					
 				}
 	        }
 	        
