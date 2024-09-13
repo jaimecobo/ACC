4bc08e8c1aa4b03e67a33a4279be3c6dfa03dd97
Jaime <jaimecobo@outlook.com>
2024-09-13 17:16:14 -0500

    Three thousand two hundred sixty ninth commit Servlet_17_Create_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_17_Create_User.java
@@ -122,7 +122,7 @@ public class Servlet_17_Create_User extends HttpServlet {
 				try {
 					String userCreated;
 		
-					sql = "insert into user (firstName, lastName, address,)"
+					sql = "insert into user (firstName, lastName, address, )"
 				}catch(SQLException ex){
 					out.println("ERROR catch in Servlet_Create_User");
 					ex.printStackTrace();
