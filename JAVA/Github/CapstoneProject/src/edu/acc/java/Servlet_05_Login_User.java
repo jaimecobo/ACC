20dc21de31995f53f44eb270f07e8f2b2d20407b
Jaime <jaimecobo@outlook.com>
2024-09-25 18:58:38 -0500

    Forty eighth commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -3,6 +3,8 @@ package edu.acc.java;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-
+/**
+ * Servlet implementation class Servlet_Login
+ */
 @WebServlet("/Login")
 public class Servlet_05_Login_User extends HttpServlet {}
