b20b4664e2190b8d0caab5d5291e14ab28703af3
Jaime <jaimecobo@outlook.com>
2024-09-26 19:06:58 -0500

    Sixty fifth commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -8,6 +8,6 @@ import javax.servlet.http.HttpServlet;
  */
 @WebServlet("/Login")
 public class Servlet_05_Login_User extends HttpServlet {
-	private static final long serialVersionUID = 1
+	private static final long serialVersionUID = 1L
 
 }
