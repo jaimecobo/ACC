ba5de73059afc34a184a8ca4364a3a62c620c1c0
Jaime <jaimecobo@outlook.com>
2024-10-18 18:25:41 -0500

    Five hundred seventeenth commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -53,7 +53,7 @@ public class Servlet_05_Login_User extends HttpServlet {
 		if (username.equals("administrator01")) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-	        Connection conn = DriverManager.getConnection("jdbc")
+	        Connection conn = DriverManager.getConnection("jdbc:")
 	        }
 		}catch (ClassNotFoundException | SQLException ex) {
         	System.out.println("ERROR in Catch");
