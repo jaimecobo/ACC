b87d547b5dd2685f5a4096d360d5f752d90008e3
Jaime <jaimecobo@outlook.com>
2024-10-20 10:51:01 -0500

    Five hundred forty seventh commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -3,7 +3,7 @@ package edu.acc.java;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java
+import java.
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
