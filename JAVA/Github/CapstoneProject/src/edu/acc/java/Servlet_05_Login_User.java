8450b9b8a3146bfc0fd9f715b1d5c947144140f7
Jaime <jaimecobo@outlook.com>
2024-10-24 18:42:34 -0500

    Six hundred thirty fourth commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -59,7 +59,8 @@ public class Servlet_05_Login_User extends HttpServlet {
 	        PreparedStatement pst = conn.prepareStatement("Select salt from user where username=?");
 	        pst.setString(1, username);
 	        ResultSet rs = pst.executeQuery();
-	        if (rs.next()) {}
+	        if (rs.next()) {
+	        }
 	        }
 		}catch (ClassNotFoundException | SQLException ex) {
         	System.out.println("ERROR in Catch");
