ea0b22ddd5494fd45ea9bb5fa54e508ccac5aad6
Jaime <jaimecobo@outlook.com>
2024-10-30 17:58:09 -0500

    Seven hundred forty eighth commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -66,6 +66,7 @@ public class Servlet_05_Login_User extends HttpServlet {
 	        	System.out.println("Salt = " + salt);
 	        }
 	        try {
+	        	
 	        }catch (NoSuchAlgorithmException ex) {
 			// TODO Auto-generated catch block
 			System.out.println(ex);
