b8cb29cf869309cae4836fe91211d7516252b904
Jaime <jaimecobo@outlook.com>
2024-11-04 18:26:30 -0600

    Eight hundred sixtieth commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -85,7 +85,7 @@ public class Servlet_05_Login_User extends HttpServlet {
 		out.println("Username = " + username);
 		out.print("<br>");
 		out.println("Password = " + password);
-		out.print("<br>")
+		out.print("<br>");
 		
 	}
 
