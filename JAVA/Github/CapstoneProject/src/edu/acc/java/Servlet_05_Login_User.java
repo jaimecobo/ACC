bda4ee105364324383f72e4d8869af4c6f0b0d17
Jaime <jaimecobo@outlook.com>
2024-11-08 08:00:05 -0600

    Nine hundred twenty fifth commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -88,7 +88,7 @@ public class Servlet_05_Login_User extends HttpServlet {
 //		out.print("<br>");
 		
 		try {
-			Class.forName("com.mysql.")
+			Class.forName("com.mysql.jdbc")
 	    }
 		catch (ClassNotFoundException | SQLException ex) {
 	        	System.out.println("ERROR in Catch");
