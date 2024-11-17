7e15fd29c740996692e4dc0e11b0eeac790a5971
Jaime <jaimecobo@outlook.com>
2024-11-16 20:56:19 -0600

    One thousand eighty first commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -95,7 +95,7 @@ public class Servlet_05_Login_User extends HttpServlet {
 	        pst.setString(2, password);
 	        ResultSet rs = pst.executeQuery();
 	        if (rs.next()) {
-	           
+	           request
 	        } 
 	    }
 		catch (ClassNotFoundException | SQLException ex) {
