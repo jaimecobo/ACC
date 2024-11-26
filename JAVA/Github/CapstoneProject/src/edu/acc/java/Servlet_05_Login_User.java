70339191dabf55caf455b1caaccc12a1f2f7a310
Jaime <jaimecobo@outlook.com>
2024-11-26 14:48:15 -0600

    One thousand three hundredth commit Servlet_05_Login_User.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/Servlet_05_Login_User.java
@@ -109,7 +109,7 @@ public class Servlet_05_Login_User extends HttpServlet {
 	        else {
 	        	String message = "Incorrect login credentials.";
 	        	request.setAttribute("message", message);
-	        	request.getRequestDispatcher("04_adminAccess.jsp").forward(request, response)
+	        	request.getRequestDispatcher("04_adminAccess.jsp").forward(request, response);
 	        
 	        }
 	    }
