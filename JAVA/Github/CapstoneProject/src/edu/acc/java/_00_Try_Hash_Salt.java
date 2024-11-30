d2077e046fb942e4f24ce6a5c96b5b1434fbb93f
Jaime <jaimecobo@outlook.com>
2024-11-30 15:23:30 -0600

    Fifty sixth commit _00_Try_Hash_Salt.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Try_Hash_Salt.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Try_Hash_Salt.java
@@ -2,6 +2,6 @@ package edu.acc.java;
 
 public class _00_Try_Hash_Salt {
 
-	static String password = "administrator"
+	static String password = "administrator";
 	public static void main (String[] args) {}
 }
