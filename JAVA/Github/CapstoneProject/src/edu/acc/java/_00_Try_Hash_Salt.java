5a10093a471670770a0b95da3159e440975a69b9
Jaime <jaimecobo@outlook.com>
2024-12-01 15:30:17 -0600

    Seventy ninth commit _00_Try_Hash_Salt.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Try_Hash_Salt.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Try_Hash_Salt.java
@@ -5,6 +5,6 @@ import javax.swing.JPanel;
 public class _00_Try_Hash_Salt {
 
 	static String password = "administrator";
-	static final JPanel panel = new JPanel
+	static final JPanel panel = new JPanel()
 	public static void main (String[] args) {}
 }
