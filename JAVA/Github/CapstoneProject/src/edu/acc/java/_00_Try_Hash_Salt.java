64743c860c439ddc81d56ccdbc4d0ad2ccf568f4
Jaime <jaimecobo@outlook.com>
2024-12-05 08:23:24 -0600

    One hundred fifty second commit _00_Try_Hash_Salt.java

--- JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Try_Hash_Salt.java
+++ JAVA/Github/CapstoneProject/src/edu/acc/java/_00_Try_Hash_Salt.java
@@ -12,7 +12,8 @@ public class _00_Try_Hash_Salt {
 //	}catch(Exception ex) {
 //		System.out.println("Exception at _00_Try_Hash_Salt checkPasswordStrength = " + ex);
 //	}
-		try {}
+		try {
+		}
 		
 	}
 }
